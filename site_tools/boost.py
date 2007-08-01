a59d860cf0177572d06c9ac163c8924b76fd1970
Gary Granger <granger@ucar.edu>
2007-08-01 23:12:11 +0000

    append a boost-specific suffix to boost library names by overriding the _LIBFLAGS construction variable to insert our own callable
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@5911 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/boost.py
+++ site_tools/boost.py
@@ -1,7 +1,21 @@
 # -*- python -*-
 import os
+import SCons.Util
 
 options = None
+
+def boost_libflags(env):
+  newlibs = []
+  for lib in env['LIBS']:
+    if SCons.Util.is_String(lib) and \
+           lib.startswith("boost_") and \
+           not lib.endswith("$BOOST_LIBRARY_SUFFIX"):
+      newlibs.append(lib+"$BOOST_LIBRARY_SUFFIX")
+    else:
+      newlibs.append(lib)
+  env['LIBS'] = newlibs
+  result = env.subst(env['_boost_save_libflags'])
+  return result
 
 def generate(env):
   global options
@@ -16,6 +30,11 @@ def generate(env):
   options.Update(env)
   env.Append(DEPLOY_SHARED_LIBS='boost_date_time')
   env.Append(DEPLOY_SHARED_LIBS='boost_serialization')
+  if not env.has_key('BOOST_LIBRARY_SUFFIX'):
+    if env['PLATFORM'] == 'win32':
+      env['BOOST_LIBRARY_SUFFIX'] = '-vc71-mt-1_33_1'
+    else:
+      env['BOOST_LIBRARY_SUFFIX'] = ''
   if env.has_key('BOOST_DIR'):
     bdir=env['BOOST_DIR']
     if bdir and bdir != "/usr" and bdir != "":
@@ -24,6 +43,13 @@ def generate(env):
       env.Append(CPPPATH=os.path.join(bdir));
       env.AppendUnique(LIBPATH=os.path.join(bdir,"lib"));
       env.AppendUnique(RPATH=[os.path.join(bdir,"lib")]);
+  # Override the _LIBFLAGS variable so we can append the suffix for
+  # boost libraries.
+  if not env.has_key('_boost_save_libflags'):
+    env["_boost_save_libflags"] = env["_LIBFLAGS"]
+    env['_LIBFLAGS'] = '${_boost_libflags(__env__)}'
+    env['_boost_libflags'] = boost_libflags
+
 
 def exists(env):
   return True
