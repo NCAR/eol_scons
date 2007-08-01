58fb1fcca216b8207c3c971a084bade933b2d1aa
Gary Granger <granger@ucar.edu>
2007-08-01 18:59:18 +0000

    extend the msvc tool to add Debug and Optimize tool settings, same as gcc does
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@5906 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/msvc.py
+++ site_tools/msvc.py
@@ -1,33 +1,31 @@
 
 import SCons.Tool
-import SCons.Tool.gcc
+import SCons.Tool.msvc
 
 def Debug(env):
-    env.Append(CCFLAGS='-g')
+    env.Append(CCFLAGS='/Zi')
     return env
 
 def Warnings(env):
-    env.Append(CCFLAGS='-Wall')
+    # env.Append(CCFLAGS='/Wall')
+    env.Append(CCFLAGS='/W2')
     if env.has_key('NOUNUSED'):
-        env.Append (CCFLAGS=['-Wno-unused'])
+        pass
     return env
 
 def Optimize(env):
-    env.Append(CCFLAGS='-O2')
+    env.Append(CCFLAGS='/O2')
     return env
 
 def Profile(env):
-    env.Append(CCFLAGS='-pg')
-    env.Append(LINKFLAGS='-pg')
-    env.Append(SHLINKFLAGS='-pg')
     return env
 
 def generate(env):
-	SCons.Tool.gcc.generate(env)
+    SCons.Tool.msvc.generate(env)
     env.Optimize = Optimize
     env.Debug = Debug
     env.Warnings = Warnings
     env.Profile = Profile
 
 def exists(env):
-    return SCons.Tool.gcc.exists(env)
+    return SCons.Tool.msvc.exists(env)
