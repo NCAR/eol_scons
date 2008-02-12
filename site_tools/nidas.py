a4cc7c4dfa3c930e0b10951da106264241a8f1fb
Gary Granger <granger@ucar.edu>
2008-02-12 20:49:49 +0000

    enhance NIDAS_PATH option to accept a comma-separated list of path prefixes to be used to link with an installed nidas and its dependencies; relative paths are automatically converted to absolute before inserting into CPPPATH and LIBPATH; if NIDAS_PATH is empty, then nidas builds are disabled, otherwise the given paths must all exist
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@6229 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/nidas.py
+++ site_tools/nidas.py
@@ -13,26 +13,35 @@ def generate(env):
     if not options:
         options = env.GlobalOptions()
         options.Add('NIDAS_PATH',
-"""Set the NIDAS install path, and enable builds of components
- which use NIDAS. Setting it to empty disables NIDAS components.""",
+"""Set the NIDAS prefix paths, and enable builds of components
+which use NIDAS. Setting it to empty disables NIDAS components.
+This can be a comma-separated list of paths, for example to build
+against a NIDAS installation whose other dependencies are installed
+under another prefix.  Relative paths will be converted to absolute
+paths relative to the top directory.""",
                     "/opt/local/nidas")
     options.Update(env)
+    env.EnableNIDAS = (lambda: 0)
+    nidas_paths = []
     if env.has_key('NIDAS_PATH') and env['NIDAS_PATH']:
-        np = env['NIDAS_PATH']
-        if os.path.isdir(np):
-            env.EnableNIDAS = (lambda: 1)
-        else:
-            env.EnableNIDAS = (lambda: 0)
-            raise NidasPathNotDirectory(
-                "Non-empty NIDAS_PATH is not a directory: %s; " % np +
-                "Disable with NIDAS_PATH=''")
-    else:
-        env.EnableNIDAS = (lambda: 0)
+        paths=env['NIDAS_PATH'].split(",")
+        env.EnableNIDAS = (lambda: 1)
+        for p in paths:
+            np = env.Dir("#").Dir(env.subst(p)).get_abspath()
+            if not os.path.isdir(np):
+                raise NidasPathNotDirectory(
+                    "Non-empty NIDAS_PATH is not a directory: %s; " % np +
+                    "Disable with NIDAS_PATH=''")
+            else:
+                nidas_paths.append(np)
+    print 'nidas_paths=', nidas_paths
     if env.EnableNIDAS():
-        env.Append(CPPPATH= ['$NIDAS_PATH/x86/include',])
-        env.Append(LIBPATH=['$NIDAS_PATH/x86/lib',])
+        env.Append(CPPPATH=[os.path.join(p,'x86','include') 
+                            for p in nidas_paths])
+        env.Append(LIBPATH=[os.path.join(p,'x86','lib') for p in nidas_paths])
         env.Append(LIBS=['nidas','nidas_dynld','nidas_util','XmlRpc'])
-        env.AppendUnique(RPATH="$NIDAS_PATH/x86/lib") 
+        env.AppendUnique(RPATH=[os.path.join(p,'x86','lib') 
+                                for p in nidas_paths])
         env.Tool("xercesc")
 
 def exists(env):
