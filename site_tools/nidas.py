2030fdab6ff515ff43f0d3ab60d28d6f7f6975ec
Gary Granger <granger@ucar.edu>
2008-02-14 19:26:18 +0000

    Not linking to nidas_util avoids multiple construction and destruction of the nidas static Thread and McSocketListener maps.  Not sure why really, but it works for now.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@6233 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/nidas.py
+++ site_tools/nidas.py
@@ -38,7 +38,14 @@ paths relative to the top directory.""",
         env.Append(CPPPATH=[os.path.join(p,'x86','include') 
                             for p in nidas_paths])
         env.Append(LIBPATH=[os.path.join(p,'x86','lib') for p in nidas_paths])
-        env.Append(LIBS=['nidas','nidas_dynld','nidas_util','XmlRpc'])
+        # At least in aeros, linking nidas_util explicitly has caused some
+        # static constructors to run multiple times (and subsequently
+        # multiple deletes).  Instead, rely on linking libnidas to link in
+        # nidas_util automatically.  There are more robust options, but this
+        # works for the moment.
+        #
+        #env.Append(LIBS=['nidas','nidas_dynld','nidas_util','XmlRpc'])
+        env.Append(LIBS=['nidas','nidas_dynld','XmlRpc'])
         env.AppendUnique(RPATH=[os.path.join(p,'x86','lib') 
                                 for p in nidas_paths])
         env.Tool("xercesc")
