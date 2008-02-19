20cb8b6f3d5a07038070f860ab6fa82c166a0156
Gary Granger <granger@ucar.edu>
2008-02-19 21:34:41 +0000

    buildmode option now takes a list of modes to apply, and it defaults to including optimization, which it should have done all along but somewhere that got dropped; this could cause some projects to rebuild completely, but in the case of projects like aeros, the result should be a much better performing executable without losing debugging info
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@6242 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/buildmode.py
+++ site_tools/buildmode.py
@@ -1,5 +1,5 @@
 
-from SCons.Script import EnumOption
+from SCons.Script import ListOption
 
 import eol_scons
 
@@ -9,19 +9,22 @@ def generate(env):
     global options
     if not options:
         options = env.GlobalOptions()
+        defaultmodes = ['debug', 'warnings', 'optimize']
+        modes = defaultmodes + ['profile']
         options.AddOptions(
-            EnumOption('buildmode',
-                       'Select basic building modes, ' +
-                       'such as for debugging or optimization.',
-                       'debug',
-                       allowed_values=('debug', 'release')))
+            ListOption('buildmode', """\
+Select basic building modes such as debugging and optimization.
+By default, all three of debugging, warnings, and optimization are enabled
+if the compiler supports it.  The modes can be selected and combined using
+a comma-separated list.""",
+                       defaultmodes, modes))
     options.Update(env)
-    if env.has_key('buildmode') and env['buildmode'] == 'release':
-        env.Tool('optimize')
-    else:
-        env.Tool('debug')
-        env.Tool('warnings')
-
+    buildmodes = env.subst("${buildmode}").split(" ")
+    for mode in buildmodes:
+        if mode == 'all':
+            env.Tool(modes)
+        elif mode != '' and mode != 'none':
+            env.Tool(mode)
 
 def exists(env):
     return True
