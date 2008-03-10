febfc10f759e63ac36f7492f87e11a2cdd13f1bc
Chris Burghart <burghart@ucar.edu>
2008-03-10 23:04:53 +0000

    Now look first for installation from the EOL xmlrpc++ RPM.  If that's not found, then fall back to assuming it's installed under OPT_PREFIX.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@6263 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/xmlrpc.py
+++ site_tools/xmlrpc.py
@@ -1,10 +1,19 @@
 import os
 
 def generate(env):
-        env.AppendUnique(CPPPATH=[os.path.join(env['OPT_PREFIX'],'include'),])
-        if not env.has_key('HAS_OPT_INCLUDE'):
-            env.Append(LIBPATH=[os.path.join(env['OPT_PREFIX'],'lib')])
-            env['HAS_OPT_INCLUDE'] = 1
+    # If the EOL xmlrpc++ RPM is installed, then headers are under 
+    # /usr/include/xmlrpc++, and we assume they are included explicitly
+    # with the xmlrpc++ directory: "#include <xmlrpc++/XmlRpc.h>".  
+    # Hence, no -I is needed in CFLAGS.  The library is in 
+    # /usr/lib/libxmlrpc++.a (or .so).
+    if (os.system('rpm -V --quiet xmlrpc++') == 0):
+        env.Append(LIBS=['xmlrpc++'])
+                         
+    # If no RPM, then assume headers are under OPT_PREFIX/include, and
+    # the library is in OPT_PREFIX/lib/libXmlRpc.a.
+    else:
+        env.AppendUnique(CPPPATH=[os.path.join(env['OPT_PREFIX'],'include')])
+        env.AppendUnique(LIBPATH=[os.path.join(env['OPT_PREFIX'],'lib')])
         env.Append(LIBS=['XmlRpc',])
 
 
