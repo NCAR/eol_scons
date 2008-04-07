1d67ff5196fe8dcd068fdfffd19d395e2d74bffb
Gary Granger <granger@ucar.edu>
2008-04-07 20:15:19 +0000

    add nidas libraries to deployed shared libraries
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@6274 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/nidas.py
+++ site_tools/nidas.py
@@ -42,7 +42,9 @@ paths relative to the top directory.""",
         # and nidas_dynld libraries need to be linked.  Linking nidas_util
         # causes static constructors to run multiple times (and
         # subsequently multiple deletes).
-        env.Append(LIBS=['nidas','nidas_dynld','XmlRpc'])
+        nidas_libs = ['nidas','nidas_dynld','XmlRpc']
+        env.Append(LIBS=nidas_libs)
+        env.AppendUnique(DEPLOY_SHARED_LIBS=nidas_libs)
         env.AppendUnique(RPATH=[os.path.join(p,'x86','lib') 
                                 for p in nidas_paths])
         env.Tool("xercesc")
