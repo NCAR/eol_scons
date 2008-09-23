99009ba47c68075641d5e128078cfb666585614d
Gary Granger <granger@ucar.edu>
2008-09-23 19:53:15 +0000

    try yet another way to add an 'install' alias automatically, this time overriding the standard Install() and InstallAs() methods so that every target with an install rule gets added to the install alias; this seems to be a cleaner and more consistent way to do it
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@6670 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/prefixoptions.py
+++ site_tools/prefixoptions.py
@@ -42,32 +42,74 @@ def OptPrefixSetup(env):
     return env
 
 
-def _InstallLibrary (self, source):
+# An 'install' alias is provided to allow the user to invoke "scons -u
+# install".  It works by adding targets to the 'install' alias when defined
+# through one of the Install methods below, *including* the standard
+# Install() and InstallAs() methods.  Several other methods have been tried
+# which did not involve overriding the standard install methods.  At one
+# point, 'install' was an alias for the root INSTALL_PREFIX path, causing
+# everything destined for that tree to be installed.  That works to a
+# point, except it would be better to add the individual install
+# subdirectories, since sometimes the INSTALL_PREFIX points to the top of
+# the source tree, in which case 'install' would become an alias to build
+# everything, even those targets not needed for an install.  However, if
+# any of the install paths do not exist or are not needed, then this scheme
+# breaks down, because scons wants the alias dependencies (the install
+# paths) to exist.  Then SCons reports an error like this:
+#
+#  scons: *** Source `conf' not found, needed by target `install'.  Stop.
+#
+# Using Ignore() does not seem to work either, because it ignores
+# dependencies of a target, but in this case we need to ignore the
+# particular source (the install directory) if there will be no installs to
+# it.
+#
+# Recent versions of scons have a FindInstalledFiles() method, and that
+# returns exactly the list of files for which we would like to add an
+# alias.  However, using that would require a hook to add the alias after
+# all the targets have been specified, ie, after all the SConscript files
+# have been read, and I cannot find any such hook in SCons yet.
+#
+# So we end up with the current scheme of 'extending' the standard Install
+# and InstallAs methods to add the 'install' alias automatically.  This
+# could break some existing eol_scons setups, but it should work more as
+# expected for those setups that count on a 'default' install alias to
+# install everything under INSTALL_PREFIX.
+
+def Install (self, *args, **kw):
+    """Add 'install' alias to targets created with standard Install() method."""
+    inst = self._prefixoptions_StandardInstall(*args, **kw)
+    self.Alias('install', inst)
+    return inst
+
+def InstallAs (self, *args, **kw):
+    """Add 'install' alias to targets created with standard Install() method."""
+    inst = self._prefixoptions_StandardInstallAs (*args, **kw)
+    self.Alias('install', inst)
+    return inst
+
+def InstallLibrary (self, source):
     """Convenience method to install a library into INSTALL_LIBDIR."""
     return self.Install (self['INSTALL_LIBDIR'], source)
 
-def _InstallPythonLibrary (self, source):
-    """Convenience method to install a python library into INSTALL_PYTHON_LIBDIR."""
+def InstallPythonLibrary (self, source):
+    """
+    Convenience method to install a python library into INSTALL_PYTHON_LIBDIR.
+    """
     return self.Install (self['INSTALL_PYTHON_LIBDIR'], source)
 
+def InstallProgram (self, source):
+    return self.Install (self['INSTALL_BINDIR'], source)
 
-def _InstallProgram (self, source):
-    inst= self.Install (self['INSTALL_BINDIR'], source)
-    return inst
+def InstallConfig (self, source):
+    return self.Install (self['INSTALL_CONFIGDIR'], source)
 
-def _InstallConfig (self, source):
-    inst= self.Install (self['INSTALL_CONFIGDIR'], source)
-    return inst
+def InstallEtc (self, source):
+    return self.Install (self['INSTALL_ETCDIR'], source)
 
-def _InstallEtc (self, source):
-    inst= self.Install (self['INSTALL_ETCDIR'], source)
-    return inst
-
-
-def _InstallHeaders (self, subdir, source):
+def InstallHeaders (self, subdir, source):
     incdir = os.path.join(self['INSTALL_INCDIR'],subdir)
     return self.Install (incdir, source)
-
 
 
 def generate(env):
@@ -88,17 +130,23 @@ def generate(env):
     env['INSTALL_ETCDIR'] = "$INSTALL_PREFIX/etc"
     env['INSTALL_PYTHON_LIBDIR'] = "$INSTALL_PREFIX/lib/python"
     # Here we install the install convenience methods, since they do not
-    # work unless the install prefix variables have been set.
-    env.InstallEtc = new.instancemethod(_InstallEtc, env, env.__class__)
-    env.InstallConfig = new.instancemethod(_InstallConfig, env, env.__class__)
-    env.InstallLibrary = new.instancemethod(_InstallLibrary, env, env.__class__)
-    env.InstallProgram = new.instancemethod(_InstallProgram, env, env.__class__)
-    env.InstallHeaders = new.instancemethod(_InstallHeaders, env, env.__class__)
-    env.InstallPythonLibrary = new.instancemethod(_InstallPythonLibrary, env, env.__class__)
-
-    # support the user invoking "scons -u install"
-    env.Alias('install',env['INSTALL_PREFIX'])
-
+    # work unless the install prefix variables have been set.  These
+    # must be set only once, else infinite recursion ensues.
+    try:
+        method = getattr(env, '_prefixoptions_StandardInstall')
+        return
+    except AttributeError:
+        pass
+    env._prefixoptions_StandardInstall = env.Install
+    env._prefixoptions_StandardInstallAs = env.InstallAs
+    env.AddMethod(Install)
+    env.AddMethod(InstallAs)
+    env.AddMethod(InstallEtc)
+    env.AddMethod(InstallConfig)
+    env.AddMethod(InstallLibrary)
+    env.AddMethod(InstallProgram)
+    env.AddMethod(InstallHeaders)
+    env.AddMethod(InstallPythonLibrary)
 
 def exists(env):
     return True
