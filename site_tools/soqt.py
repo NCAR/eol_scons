c7e6947a6bf8d79733c0858978099545e6657f0e
Gary Granger <granger@ucar.edu>
2008-12-06 06:19:52 +0000

    try to remove the qt4 include and library paths from soqt-config in favor of the paths specified by the qt4 tool
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@6726 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/soqt.py
+++ site_tools/soqt.py
@@ -4,6 +4,8 @@ from eol_scons import parseconfig
 
 _options = None
 
+_removed_already_warned = False
+
 def getPrefix(env, apply_config = False):
     matchdir = env.FindPackagePath('SOQT_DIR','$OPT_PREFIX/SoQt*')
     prefixes = [ env.get('SOQT_DIR'), matchdir, env.get('COIN_DIR'),
@@ -23,6 +25,39 @@ Use the first soqt-config found in this list of paths:
                     getPrefix(env))
     _options.Update(env)
     prefix = getPrefix(env, apply_config = True)
+
+    # As a special case, if QT4DIR is not /usr/lib64/qt4 or /usr/lib/qt4,
+    # then remove all elements from CPPPATH and LIBPATH with those paths.
+    # SoQt may have been built against a different Qt4, such as the one
+    # installed in the system, but this tool needs to force the Qt4
+    # configuration chosen here.
+
+    def find_path(paths, p):
+        for i in range(0, len(paths)):
+            if paths[i].find(p) >= 0:
+                return i
+        return -1
+
+    def remove_path(paths, p):
+        n = 0
+        i = find_path(paths, p)
+        while i >= 0:
+            del paths[i]
+            n += 1
+            i = find_path(paths, p)
+        return n
+
+    removed = 0
+    if env.get('QT4DIR') != "/usr/lib64/qt4":
+        removed += remove_path(env['CPPPATH'], "/usr/lib64/qt4")
+        removed += remove_path(env['LIBPATH'], "/usr/lib64/qt4")
+    if env.get('QT4DIR') != "/usr/lib/qt4":
+        removed += remove_path(env['CPPPATH'], "/usr/lib/qt4")
+        removed += remove_path(env['LIBPATH'], "/usr/lib/qt4")
+    global _removed_already_warned
+    if removed > 0 and not _removed_already_warned:
+        print("Removed %d extraneous qt4 paths from soqt-config." % removed)
+        _removed_already_warned = True
 
     if not env.has_key('SOQT_DOXDIR'):
         # When installed into the system as the SoQt-devel package,
