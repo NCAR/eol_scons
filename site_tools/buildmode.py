a2ed8938035c54ad0a21b823ea35260d1d008e91
Gary Granger <granger@ucar.edu>
2009-07-30 16:14:45 +0000

    add thoughts about reworking this tool
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@7006 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/buildmode.py
+++ site_tools/buildmode.py
@@ -1,3 +1,23 @@
+"""
+Accept a list of known 'build modes', such as optimization and compiler
+warnings, which are mapped to different compiler options on different
+platforms.  The mapping happens by applying each mode as a tool, and the
+tool in turn tries to apply the right method from the Environment instance.
+Different platform compiler tools, such as gcc and msvc, provide the
+environment methods which set the right compiler flags for the build mode.
+
+This seems more circuitous than necessary.  Another option is for a build
+mode to just be its own tool, which tests the current platform to decide
+what options to apply.  However, it makes more object-oriented sense to
+group the settings in the compiler tool.  The specific build mode tools
+like warnings.py and optimize.py could be removed in favor of calling
+directly the right method on the environment instance based on the
+buildmode name.
+
+The problem then is how to specify options and defaults to buildmode, since
+it is applied when it is loaded.
+
+"""
 
 from SCons.Script import ListVariable
 
