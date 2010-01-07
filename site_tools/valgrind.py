a612ada5098a20e70d009cf6ae30ffcedb856320
Gary Granger <granger@ucar.edu>
2010-01-07 18:25:26 +0000

    commit some ideas, even though nothing's been done to implement them
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@7298 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/valgrind.py
+++ site_tools/valgrind.py
@@ -1,6 +1,58 @@
-# Simple scons tool to find a valgrind executable and set the VALGRIND_PATH
-# construction variable accordingly.  The value will also be available in
-# the runtime environment, such as for test scripts executed by scons.
+"""\
+Simple scons tool to find a valgrind executable and set the VALGRIND_PATH
+construction variable accordingly.  The value will also be available in
+the runtime environment, such as for test scripts executed by scons.
+
+Also set VALGRIND_COMMAND according to these options provided to the
+scons environment.  If valgrind is 'off', then VALGRIND_COMMAND will be
+empty.  Set VALGRIND_VERSION to the valgrind version string
+using 'valgrind --version'.
+
+ valgrind={valgrind|callgrind|off}    
+    Whether to run valgrind or callgrind or neither.
+ 
+The valgrind Action runs another Action using valgrind
+
+These are the expected uses of this tool:
+
+ * Run a test script which runs a test program using VALGRIND_COMMAND:
+     $VALGRIND_COMMAND ./test_program <program_options>   > test.log
+   The script can add a suppressions file if it likes:
+     if [ -n "$VALGRIND_COMMAND" ]; then
+        vgopts="--suppressions=vg.suppresions"
+     fi
+ * Build a summary report of a valgrind log file which results in a failure
+   when certain errors are detected.
+     env.ValgrindLog("test.log", VALGRIND_LEAK_THRESHOLD=0, 
+                                 VALGRIND_ERROR_THRESHOLD=0)
+
+So everything can be combined with this:
+
+   env.Valgrind("test_command ...options...", VALGRIND_ settings...)
+
+or
+
+   env.ValgrindLog(env.Test(...), VALGRIND_ settings...)
+
+The Valgrind() builder first wraps the command in a Test() builder,
+then parses the output.
+
+If valgrind is not enabled, then ValgrindLog() and Valgrind() do not do
+anything except run the test program.
+
+Add a TEST_LOGFILE construction variable for Test() pseudo-builder, so the
+logfile will be the target of the Test().  Can xtest still be an alias to
+run the test in that case?
+
+Valgrind builders should be added to the test alias too?  Or perhaps I have
+this backwards: the Test() pseudo-builders should add the valgrind actions
+instead of the other way around, eg, env.Test("command") will call the
+valgrind builder automatically if enabled.  The test should run and have
+the 'test' and 'xtest' aliases whether valgrind is run as part of it or
+not.
+
+
+"""
 
 import os
 import re
