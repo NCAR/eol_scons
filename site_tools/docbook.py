8a0f61d762557c110d2298ef5fb7512ca0c2d8e1
Gary Granger <granger@ucar.edu>
2010-02-09 20:26:53 +0000

    move docbook builder tools from aeros/doc/design into eol scons, mostly because they are needed by the iss tree
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@7352 da89c675-a4d7-0310-8a15-9e7546503515

---
+++ site_tools/docbook.py
@@ -0,0 +1,85 @@
+# SCons tool to add builders for rendering and publishing docbook files.
+#
+# A couple options have been used over the years.  One is the jw package
+# based on Jade and OpenJade and DSSSL (.dsl) style sheets, whose entry
+# points are scripts like docbook2pdf (db2pdf) and docbook2html (db2html).
+# The other option is xmlto and XSL stylesheets.  On Fedora, there are
+# three xmlto packages to install.
+#
+# xmlto
+# xmlto-xhtml
+# xmlto-tex
+#
+# xmlto-xhtml may not be required for pdf and html output, but it's included
+# for good measure.
+
+
+import glob
+import os
+import SCons
+
+from SCons.Script import Builder
+
+
+def xmltohtml_emitter (target, source, env):
+    outputdir = str(target[0].get_dir())
+    # target = [ env.Dir (os.path.join(outputdir,"html")) ]
+    target = [ env.File (os.path.join(outputdir,"html","index.html")) ]
+    # print "emitter returning ", str(target[0]), str(source[0])
+    return target, source
+
+
+# def make_dtd_link(target, source, env):
+#   xlink = str(target[0])
+#   dirs = glob.glob(str(source[0])+"/xml-dtd-4.2*")
+#   dirs.sort()
+#   if len(dirs) == 0:
+#       raise "Could not find an xml-dtd installation in %s" % str(source[0])
+#   print "Linking %s to %s" % (dirs[0], xlink)
+#   try:
+#       os.unlink (xlink)
+#   except:
+#       pass
+#   os.symlink (dirs[0], xlink)
+#   return None
+
+
+db2pdf = Builder(action = 'HOME=$HOME $DOCBOOK2PDF $SOURCE',
+                 suffix = '.pdf',
+                 src_suffix = '.xml')
+xmltopdf = Builder(action = '$XMLTO pdf $SOURCE',
+                   suffix = '.pdf',
+                   src_suffix = '.xml')
+xmltohtml1 = Builder(action = '$XMLTO html-nochunks -o $TARGET.dir $SOURCE',
+                     suffix = '.html',
+                     src_suffix = '.xml')
+xmltohtml = Builder(action = '$XMLTO html -o $TARGET.dir $SOURCE',
+                    emitter = xmltohtml_emitter,
+                    src_suffix = '.xml')
+
+
+def publish_docbook(env, name, pubdir):
+    html1 = env.xmltohtml1([name])
+    html = env.xmltohtml([name])
+    env.Clean(html, [env.Dir("html")])
+    pdf = env.xmltopdf([name, "print.xsl"])
+    htmlinstall = env.Install(os.path.join(pubdir,"html"), html)
+    env.AddPostAction(htmlinstall, "cp -r $SOURCE.dir/. $TARGET.dir")
+    pdfinstall = env.Install(pubdir, pdf)
+    html1install = env.Install(pubdir, html1)
+    return [pdfinstall, htmlinstall, html1install]
+
+
+def generate(env):
+
+    env.SetDefault(XMLTO='xmlto')
+    env.SetDefault(DOCBOOK2PDF='docbook2pdf')
+    env.SetDefault(HOME=os.environ['HOME'])
+    env['BUILDERS']['xmltohtml1'] = xmltohtml1
+    env['BUILDERS']['xmltohtml'] = xmltohtml
+    env['BUILDERS']['xmltopdf'] = xmltopdf
+    env.AddMethod(publish_docbook, 'PublishDocbook')
+
+
+def exists(env):
+    return env.Detect('xmlto')
