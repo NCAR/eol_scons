41b9e6c90cf4bb0b078c1deca8d7071df54b4add
Chris Burghart <burghart@ucar.edu>
2010-10-27 18:44:20 +0000

    Older versions of boost::mutex require linking against libboost_thread and libpthread, while newer versions only require libpthread. Use SCons.Environment.Configure to determine which one actually makes things work, and go with that.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@7791 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/boost_mutex.py
+++ site_tools/boost_mutex.py
@@ -1,10 +1,29 @@
 import os,os.path, sys
 import SCons
 
+
 # Use this tool for all Boost mutex classes (I'm using it for 
-# boost::recursive_mutex); the Boost mutex classes depend on libpthread...
+# boost::recursive_mutex). The mutex classes always require libpthread,
+# and depending on Boost version, may also require libboost_thread.
 def generate(env):
-    env.Append(LIBS=['pthread',])
+    # For older versions of Boost mutexes, we need to link with both
+    # boost_thread and pthread libraries. For later versions, we only
+    # need pthread. Use Environment.Configure to test which way will work
+    # on this system.
+    clone = env.Clone()
+    clone.Replace(LIBS=[])
+    conf = clone.Configure()
+    header = 'boost/thread/mutex.hpp'
+    test_src = 'boost::mutex m; boost::mutex::scoped_lock guard(m);'
+    if (not conf.CheckLibWithHeader('pthread', header, 'CXX', test_src) and
+        not conf.CheckLibWithHeader(['boost_thread', 'pthread'], header, 'CXX', test_src)):
+            msg = "Failed to link to boost::mutex both with and without"
+            msg += " libboost_thread.  Check config.log."
+            raise SCons.Errors.StopError, msg
+
+    conf.Finish()
+        
+    env.Append(LIBS=clone['LIBS'])
 
 def exists(env):
     return True
