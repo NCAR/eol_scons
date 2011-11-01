d7f84bcc94fccd92811a954f1f03e24ac74d8a4b
Gordon Maclean <maclean@ucar.edu>
2011-11-01 20:10:48 +0000

    Another hack on SharedLibrary3 builder. The previous version called the SCons SharedLibrary builder from the SharedLibrary3 emitter, which worked mostly, but is apparently non-standard.  If the builder was invoked multiple times in a build, the environment used when the action was executed did not seem to be updated by things like LIBS="xyz" in the call to the builder.
    This new version borrows a bunch from the SharedLibrary builder, using its
    action.
    
    
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@8110 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/sharedlibrary.py
+++ site_tools/sharedlibrary.py
@@ -1,165 +1,63 @@
 import os
+import re
 from symlink import MakeSymLink
 from SCons.Node.FS import Dir,File
-from SCons.Script import Configure,Builder,Action,Mkdir,Touch
+from SCons.Script import Configure,Builder,Execute
+import SCons.Defaults
+import SCons.Scanner.Prog
 
 def SharedLibrary3Emitter(target,source,env):
-    """ 
-        For a reference on Linux conventions for shared library names see
-        http://tldp.org/HOWTO/Program-Library-HOWTO/shared-libraries.html
-
-        The usual convention is that the full library name is something like:
-            libxxx.so.X.Y
-        Where X is the major version number of the binary API and Y is the minor version.
-
-        The SONAME of the library is
-            libxxx.so.X
-        The basic library name, used when initially linking executables, is
-            libxxx.so
-
-        Under linux, libxxx.so.X.Y is typically the actual library file,
-        and libxxx.so.X and libxxx.so are symbolic links.
-
-        The idea is that two libraries with the same name, same major
-        number, but differing minor number, implement the same binary API, and
-        that a library with a new minor number could replace the old
-        without breaking executable programs that depend on the library.
-
-        From the man page of ld, discussing the -soname option:
-        -soname=name
-            When creating an ELF shared object, set the internal DT_SONAME field to
-            the specified name.  When an executable is linked with a shared object
-            which has a DT_SONAME field, then when the executable is run the dynamic
-            linker will attempt to  load  the  shared object specified by the
-            DT_SONAME field rather than the using the file name given to the linker.
-
-        If the SONAME of a library contains just the major version number, and a
-        a symbolic link exists with a name equal to the SONAME, pointing
-        to the real library file, then a new library file could be installed
-        with a different minor number, and the symbolic link updated to point
-        to the new library, without re-linking executables.  This will only work
-        if the binary APIs of libraries with the same major number are compatible.
-
-        The SONAME of a library can be seen with
-            objdump -p libxxx.so | grep SONAME
-
-        ldd lists the SONAMEs of the libraries that a program was linked against.
-        rpmbuild uses the same tools as ldd, and creates dependencies for
-        executables based on the SONAMEs of the linked libraries.
-
-        If the library has a SONAME, then the basic library name, libxxx.so,
-        without major and minor numbers, is only used when linking executables
-        with the -lxxx option.  That is why symbolic link .so's, without major
-        and minor numbers, are sometimes found only in -devel RPMs.
-
-        To create the above three libraries with this builder, do:
-
-            env['SHLIBMAJORVERSION'] = '3'
-            env['SHLIBMINORVERSION'] = '4'
-            lib = env.SharedLibrary3('xxx',objects)
-
-        This builder will set the -soname in the real library file, and the other
-        two will be symbolic links.
-
-        To install the library and the symbolic links to a destination:
-
-            env.SharedLibrary3Install('/opt/mystuff',lib)
-
-        If the environment token USE_ARCHLIBDIR is not defined, the
-        libraries will be installed to /opt/mystuff/lib.  If USE_ARCHLIBDIR
-        is defined, the libraries will be installed in /opt/mystuff/env['ARCHLIBDIR'].
-        ARCHLIBDIR will have a value of "lib64" on linux 64 bit systems, otherwise
-        "lib".
-
-        As of this writing, this builder has only been tested on Linux.
-        Support for other architectures needs to be added as necessary.
-    """
 
     """
-    In this emitter for SharedLibary3, target is the desired name of
-    the library, like 'xxx'.
-    source is the list of source or object files.
+    If 'xxx' was the target name passed to the SharedLibrary3 builder,
+    then the target passed to this emitter is libxxx.so.X.Y, assuming
+    $SHLIBPREFIX='lib', is the value of the prefix parameter when this
+    builder was created, and the suffix is '.so.X.Y', the concatenation of
+    $SHLIBSUFFIX + '.' + $SHLIBMAJORVERSION + '.' + $SHLIBMINORVERSION
 
-    Two construction variables must be defined for this builder:
-    SHLIBMAJORVERSION and SHLIBMINORVERSION, which in the following
-    examples are 'X' and 'Y'.
-    
-    This emitter registers a SharedLibrary builder for a target 'xxx' with
-    the given source list of object and source files, link flags of
-    '-Wl,-soname=libxxx.so.X' a and a shared library suffix of
-    '.so.X.Y'. The SharedLibrary builder returns a target with the
-    full name of 'libxxx.so.X.Y'
-
-    This emitter then registers a symbolic link builder to link
-    libxxx.so.X.Y to the soname libxxx.so.X.
-
-    It then emits a target dependency of libxxx.so, and a source dependency
-    of libxxx.so.X.Y. The target dependency is necesary if the user wants
-    to do more with the library, such as install it with SharedLibrary3Install.
-    
-    The action of this builder then just has to complete the symbolic link
-    from libxxx.so.X.Y to libxxx.so.
-
-    If you try to register a symbolic link builder for libxxx.so in this
-    emitter, you'll get a warning:
-        Two different environments were specified for target libxxx.so,
-        but they appear to have the same action:
-    So we have to do the symbolic link in the action.
+    This emitter returns three targets for the given sources,
+    in this order:
+        libxxx.so, libxxx.so.X, and libxxx.so.X.Y
     """
 
-    # libxxx.so
-    # env['SHLIBPREFIX'] becomes $LIBPREFIX so use env.subst()
-    libname = env.subst('$SHLIBPREFIX') + str(target[0]) + env['SHLIBSUFFIX']
+    fullname = str(target[0])
 
+    # Use reg expression substitution to remove the '.X.Y' from 'libxxx.so.X.Y'
     try:
-        # libxxx.so.X
-        soname = libname + '.' + env['SHLIBMAJORVERSION']
+        # Convert '.' to '\.' for exact matching.
+        versionsuffixre = re.sub(r'\.',r'\\.',
+                '.' + env['SHLIBMAJORVERSION'] + '.' + env['SHLIBMINORVERSION'] + '$')
     except KeyError:
-        print 'Cannot find SHLIBMAJORVERSION env variable'
+        print 'Cannot find SHLIBMAJORVERSION or SHLIBMINORVERSION env variables'
 
-    try:
-        fulllib = env.SharedLibrary(str(target[0]),source,
-            SHLINKFLAGS = env['SHLINKFLAGS'] + ['-Wl,-soname=' + soname],
-            SHLIBSUFFIX = env['SHLIBSUFFIX'] +
-                '.' + env['SHLIBMAJORVERSION'] +
-                '.' + env['SHLIBMINORVERSION'])
-    except KeyError:
-        print 'Cannot find SHLIBMINORVERSION env variable'
-        return None
+    libname = re.sub(versionsuffixre,'', fullname)
+    # print "fullname=" + fullname + ", libname=" + libname
 
-    # For some reason we have to create the original target, otherwise:
-    # scons: *** [x86] Implicit dependency `build_x86/util/nidas_util' not found,
-    # needed by target `x86'.
-    # 'util' is the directory containing the SConscript, whose variant_dir is
-    # 'build_util'.
-    # Creating an empty file named str(target[0]) on the variant_dir
-    # satisfies scons.
+    soname = libname + '.' + env['SHLIBMAJORVERSION']
 
-    env.Command(target,source,
-        [Mkdir('$TARGET.dir'),Touch('$TARGET')])
-
-    # Register builder to create link to soname
-    env.SymLink(soname,fulllib)
-
-    # Don't register a builder here to create libname, like so:
-    # env.SymLink(libname,fullname)
-    # because you'll get a warning:
-    # Two different environments were specified for target libxxx.so,
-    #         but they appear to have the same action:
-
-    return ([libname],[fulllib])
+    return ([libname,soname,fullname],source)
 
 def SharedLibrary3Action(target,source,env):
-    # This action just has to make the final symbolic link from
-    # libxxx.so.X.Y to libxxx.so. Note it is done with an action,
-    # not a builder, which would have created more dependencies.
-    MakeSymLink(target,source,env)
 
-    # If we ever want to completely replace the action of
-    # SharedLibrary, here is its action, as found in
-    # /usr/lib/scons/SCons/Defaults.py
-    # ShLinkAction = SCons.Action.Action("$SHLINKCOM","$SHLINKCOMSTR")
+    """
+    Action to build a shared library and symbolic links.
 
+    The targets generated by the above emitter are:
+    target[0] = libxxx.so       short name
+    target[1] = libxxx.so.X     soname
+    target[2] = libxxx.so.X.Y   full library
+
+    SHLINKCOM is the action used by the SharedLibrary builder.
+    """
+
+    # Execute the shared library action to build the full library
+    env.Execute(env.subst('$SHLINKCOM',target=target[2],source=source),
+        env.subst('$SHLINKCOMSTR',target=target[2],source=source))
+
+    # Now use the action of the SymLink builder to create
+    # symbolic links from libxxx.so.X.Y to libxxx.so.X and libxxx.so
+    MakeSymLink([target[0]],[target[2]],env)
+    MakeSymLink([target[1]],[target[2]],env)
     return 0
 
 def SharedLibrary3Install(env,target,source,**kw):
@@ -174,13 +72,21 @@ def SharedLibrary3Install(env,target,source,**kw):
     See the discussion for SharedLibrary3 about how library versions 
     are handled.
 
-    source should be a path like libxxx.so, which is what is returned
-    as a target by the SharedLibrary3 builder.
+    source[0] is the target returned from SharedLibrary3, which is the
+    short name of the library: libxxx.so
 
-    This installer will copy libxxx.so.$SHLIBMAJORVERSION.$SHLIBMINORVERSION
-    to target/lib[64], and then create symbolic links on the target library directory:
-        libxxx.so -> libxxx.so.$SHLIBMAJORVERSION.$SHLIBMINORVERSION
-        libxxx.so.$SHLIBMAJORVERSION -> libxxx.so.$SHLIBMAJORVERSION.$SHLIBMINORVERSION
+    If source[1] exists, it is assumed to be the second target
+    returned from SharedLibrary3, which is the "soname":
+    libxxx.so.X
+
+    If source[2] exists, it is assumed to be the third target
+    returned from SharedLibrary3, which is the full library name:
+    libxxx.so.X.Y
+
+    This installer will copy libxxx.so.X.Y to target/lib[64],
+    and then create symbolic links on the target lib directory:
+        libxxx.so -> libxxx.so.X.Y
+        libxxx.so.X -> libxxx.so.X.Y
     """
 
     # add passed keywords to environment
@@ -193,19 +99,25 @@ def SharedLibrary3Install(env,target,source,**kw):
 
     # libname = source[0].path
     libname = str(source[0])
-    try:
-        soname = libname + '.' + env['SHLIBMAJORVERSION']
-    except KeyError:
-        print 'Cannot find SHLIBMAJORVERSION env variable'
-        return None
-    # print "SharedLibrary3Install, soname=" + soname
 
-    try:
-        fullname = soname + '.' + env['SHLIBMINORVERSION']
-    except KeyError:
-        print 'Cannot find SHLIBMINORVERSION env variable'
-        return None
-    # print "SharedLibrary3Install, fullname=" + fullname
+    if len(source) > 1:
+        soname = str(source[1])
+    else:
+        try:
+            soname = libname + '.' + env['SHLIBMAJORVERSION']
+        except KeyError:
+            print 'Cannot find SHLIBMAJORVERSION env variable'
+            return None
+
+    if len(source) > 2:
+        fullname = str(source[2])
+    else:
+        try:
+            fullname = soname + '.' + env['SHLIBMINORVERSION']
+        except KeyError:
+            print 'Cannot find SHLIBMINORVERSION env variable'
+            return None
+        # print "SharedLibrary3Install, fullname=" + fullname
 
     nodes = []
     tgt = env.Install(targetDir,fullname)
@@ -221,9 +133,118 @@ def SharedLibrary3Install(env,target,source,**kw):
     return nodes
 
 def generate(env):
+    """ 
+    Builder and Installer for shared libraries and their associated symbolic links.
 
-    # Add builder and installer for shared libraries
-    builder = Builder(action=SharedLibrary3Action,emitter=SharedLibrary3Emitter)
+    Usage:
+        env.Append(SHLIBMAJORVERSION = '3')
+        env.Append(SHLIBMINORVERSION = '5')
+        env.Append(USE_ARCHLIBDIR = 1)
+
+        # if modules in libfoo.so use symbols from libbar.so, add LIBS=['bar']
+        libs = env.SharedLibrary3('foo',['foo.c'],
+            LIBS=['bar'],LIBPATH=['/opt/bar/lib'])
+        env.SharedLibrary3Install('/opt/foo',libs)
+
+    The SharedLibrary3 builder above will create one library and two
+    symbolic links to it:
+    1. actual library file, libfoo.so.3.5
+    2. symbolic link: libfoo.so.3 -> libfoo.so.3.5
+    3. symbolic link: libfoo.so -> libfoo.so.3.5
+
+    The SharedLibrary3Install statement will create that same file
+    and links to it on /opt/foo/lib or /opt/foo/lib64, depending
+    on whether the build system is 64 big.
+
+    If USE_ARCHLIBDIR is 0, then the libraries will be installed on
+    /opt/foo/lib, in either 32 or 64 bit systems.
+
+    For a reference on Linux conventions for shared library names see
+    http://tldp.org/HOWTO/Program-Library-HOWTO/shared-libraries.html
+
+    The usual convention is that the full library name is something like:
+        libxxx.so.X.Y
+    Where X is the major version number of the binary API and Y is the minor version.
+
+    The SONAME of the library is
+        libxxx.so.X
+    The basic library name, used when initially linking executables, is
+        libxxx.so
+
+    Under linux, libxxx.so.X.Y is typically the actual library file,
+    and libxxx.so.X and libxxx.so are symbolic links.
+
+    The idea is that two libraries with the same name, same major
+    number, but differing minor number, implement the same binary API, and
+    that a library with a new minor number could replace the old
+    without breaking executable programs that depend on the library.
+
+    From the man page of ld, discussing the -soname option:
+    -soname=name
+        When creating an ELF shared object, set the internal DT_SONAME field to
+        the specified name.  When an executable is linked with a shared object
+        which has a DT_SONAME field, then when the executable is run the dynamic
+        linker will attempt to  load  the  shared object specified by the
+        DT_SONAME field rather than the using the file name given to the linker.
+
+    If the SONAME of a library contains just the major version number, and a
+    a symbolic link exists with a name equal to the SONAME, pointing
+    to the real library file, then a new library file could be installed
+    with a different minor number, and the symbolic link updated to point
+    to the new library, without re-linking executables.  This will only work
+    if the binary APIs of libraries with the same major number are compatible.
+
+    The SONAME of a library can be seen with
+        objdump -p libxxx.so | grep SONAME
+
+    ldd lists the SONAMEs of the libraries that a program was linked against.
+    rpmbuild uses the same tools as ldd, and creates dependencies for
+    executables based on the SONAMEs of the linked libraries.
+
+    If the library has a SONAME, then the basic library name, libxxx.so,
+    without major and minor numbers, is only used when linking executables
+    with the -lxxx option.  That is why symbolic link .so's, without major
+    and minor numbers, are sometimes found only in -devel RPMs.
+
+    To create the above three libraries with this builder, do:
+
+        env['SHLIBMAJORVERSION'] = '3'
+        env['SHLIBMINORVERSION'] = '4'
+        lib = env.SharedLibrary3('xxx',objects)
+
+    This builder will set the -soname in the real library file, and the other
+    two will be symbolic links.
+
+    To install the library and the symbolic links to a destination:
+
+        env.SharedLibrary3Install('/opt/mystuff',lib)
+
+    If the environment token USE_ARCHLIBDIR is not defined, the
+    libraries will be installed to /opt/mystuff/lib.  If USE_ARCHLIBDIR
+    is defined, the libraries will be installed in /opt/mystuff/env['ARCHLIBDIR'].
+    ARCHLIBDIR will have a value of "lib64" on linux 64 bit systems, otherwise
+    "lib".
+
+    As of this writing, this builder has only been tested on Linux.
+    Support for other architectures needs to be added as necessary.
+
+    One potential issue is whether there should be dots between the
+    library suffix and the major and minor version numbers in the
+    library file name.
+    """
+
+    # Special builder for shared libraries.
+    # Some of these build parameters were stolen from the definition
+    # of the SharedLibrary builder in /usr/lib/scons/SCons/Tool/__init__.py
+    builder = Builder(
+            action=[SCons.Defaults.SharedCheck,SharedLibrary3Action],
+            emitter=SharedLibrary3Emitter,
+            prefix='$SHLIBPREFIX',
+            suffix='${SHLIBSUFFIX}.${SHLIBMAJORVERSION}.${SHLIBMINORVERSION}',
+            target_scanner=SCons.Scanner.Prog.ProgramScanner(),
+            src_suffix='$SHOBJSUFFIX',
+            src_builder='SharedObject'
+            )
     env.Append(BUILDERS = {"SharedLibrary3": builder})
 
     env.AddMethod(SharedLibrary3Install)
