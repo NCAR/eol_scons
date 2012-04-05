51890c07c27a1903a3b787c3ea9e15871ba761ef
Gordon Maclean <maclean@ucar.edu>
2012-04-05 21:56:29 +0000

    added pkg-config to nidas tool
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@8263 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/nidas.py
+++ site_tools/nidas.py
@@ -40,46 +40,55 @@ This can be a comma-separated list of paths, for example to build
 against a NIDAS installation whose other dependencies are installed
 under another prefix.  Relative paths will be converted to absolute
 paths relative to the top directory.""",
-                    "/opt/local/nidas")
+                    "/opt/nidas")
     _options.Update(env)
     env.EnableNIDAS = (lambda: 0)
     nidas_paths = []
-    if env.has_key('NIDAS_PATH') and env['NIDAS_PATH']:
-        paths=env['NIDAS_PATH'].split(",")
-        for p in paths:
-            np = env.Dir("#").Dir(env.subst(p)).get_abspath()
-            if not os.path.isdir(np):
-                if not _warned_paths.has_key(np):
-                    print "NIDAS path is not a directory: " + np
-                _warned_paths[np] = 1
-            else:
-                nidas_paths.append(np)
-        if len(nidas_paths) == 0:
-            raise NidasPathNotDirectory(
-                "No directories found in NIDAS_PATH: %s; " % \
-                    env['NIDAS_PATH'] + "disable NIDAS with NIDAS_PATH=''")
-        env.EnableNIDAS = (lambda: 1)
-    if env.EnableNIDAS():
-        env.Append(CPPPATH=[os.path.join(p,'x86','include') 
-                            for p in nidas_paths])
-        libdir = _get_libsubdir(env)
-        env.Append(LIBPATH=[os.path.join(p, 'x86', libdir) 
-                            for p in nidas_paths])
-        # The nidas library contains nidas_util already, so only the nidas
-        # and nidas_dynld libraries need to be linked.  Linking nidas_util
-        # causes static constructors to run multiple times (and
-        # subsequently multiple deletes).
-        nidas_libs = ['nidas','nidas_dynld','nidas_util']
-        env.Append(LIBS=nidas_libs)
-        env.AppendUnique(DEPLOY_SHARED_LIBS=nidas_libs)
-        env.AppendUnique(RPATH=[os.path.join(p, 'x86', libdir)
+    if env.has_key('NIDAS_PATH'):
+        if env['NIDAS_PATH']:
+            paths=env['NIDAS_PATH'].split(",")
+            for p in paths:
+                np = env.Dir("#").Dir(env.subst(p)).get_abspath()
+                if not os.path.isdir(np):
+                    if not _warned_paths.has_key(np):
+                        print "NIDAS path is not a directory: " + np
+                    _warned_paths[np] = 1
+                else:
+                    nidas_paths.append(np)
+            if len(nidas_paths) == 0:
+                raise NidasPathNotDirectory(
+                    "No directories found in NIDAS_PATH: %s; " % \
+                        env['NIDAS_PATH'] + "disable NIDAS with NIDAS_PATH=''")
+            env.EnableNIDAS = (lambda: 1)
+            env.Append(CPPPATH=[os.path.join(p,'x86','include') 
                                 for p in nidas_paths])
-        # Anything using nidas is almost guaranteed now to have to link
-        # with xerces.  Including some of the nidas headers creates direct
-        # dependencies on xercesc symbols, even though an application may
-        # not actually make any xercesc calls.  Such shared library
-        # dependencies now have to be linked explicitly.
-        env.Tool("xercesc")
+            libdir = _get_libsubdir(env)
+            env.Append(LIBPATH=[os.path.join(p, 'x86', libdir) 
+                                for p in nidas_paths])
+            # The nidas library contains nidas_util already, so only the nidas
+            # and nidas_dynld libraries need to be linked.  Linking nidas_util
+            # causes static constructors to run multiple times (and
+            # subsequently multiple deletes).
+            nidas_libs = ['nidas','nidas_dynld','nidas_util']
+            env.Append(LIBS=nidas_libs)
+            env.AppendUnique(DEPLOY_SHARED_LIBS=nidas_libs)
+            env.AppendUnique(RPATH=[os.path.join(p, 'x86', libdir)
+                                    for p in nidas_paths])
+            # Anything using nidas is almost guaranteed now to have to link
+            # with xerces.  Including some of the nidas headers creates direct
+            # dependencies on xercesc symbols, even though an application may
+            # not actually make any xercesc calls.  Such shared library
+            # dependencies now have to be linked explicitly.
+            env.Tool("xercesc")
+    else:
+        try:
+            env.EnableNIDAS = (lambda: (os.system('pkg-config --exists nidas') == 0))
+        except:
+            pass
+        if env.EnableNIDAS():
+            # Don't try here to make things unique in CFLAGS; just do an append
+            env.ParseConfig('pkg-config --cflags nidas', unique = False)
+            env.ParseConfig('pkg-config --libs nidas', unique = False)
         env.Tool('xmlrpc')
     
 
