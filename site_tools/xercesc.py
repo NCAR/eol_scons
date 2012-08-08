599ae7f91a57e084aae02d5b1a704df88ec59dfc
Gary Granger <granger@ucar.edu>
2012-08-08 00:01:44 +0000

    Do not add OPT_PREFIX/lib to the libpath, even if set, unless the xerces-c library exists there.  This prevents it from interfering with other tools and projects like nidas which do not use prefixoptions.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@8374 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/xercesc.py
+++ site_tools/xercesc.py
@@ -59,18 +59,22 @@ Requires the xerces-c27-devel package to be installed.""",
         _options.Update(env)
         x27 = env['xercesc27']
         if env.has_key('XERCESC_PREFIX'):
-            prefix = env['XERCESC_PREFIX']
+            prefix = env.subst(env['XERCESC_PREFIX'])
         elif env.has_key('OPT_PREFIX') and not x27:
-            prefix = env['OPT_PREFIX']
+            prefix = env.subst(env['OPT_PREFIX'])
             env['XERCESC_PREFIX'] = prefix
         self.checkBuild(env)
         if self.building:
             env['XERCESCROOT'] = self.getPackagePath(env)
             env.Append(LIBS=[env.GetGlobalTarget('libxerces-c'),])
         else:
-            if prefix:
-                env.AppendUnique(LIBPATH=[os.path.join(prefix,'lib'),])
             env.Append(LIBS=['xerces-c'])
+            if bool(prefix and
+                    os.path.exists(os.path.join(prefix,
+                                                'lib', 'libxerces-c.so'))):
+                env.AppendUnique(LIBPATH=[os.path.join(prefix, 'lib')])
+                env.AppendUnique(CPPPATH=[os.path.join(prefix, 'include'),])
+
             # Supply a hard-coded default for finding doxygen docs
             if not env.has_key('XERCESCROOT'):
                 env['XERCESCROOT'] = '/net/src/prog-tools/xerces-c-src_2_6_0'
@@ -79,9 +83,6 @@ Requires the xerces-c27-devel package to be installed.""",
             env['XERCESC_DOXDIR'] = "%s/doc/html/apiDocs" % env['XERCESCROOT']
         doxref= "xercesc:%s" % env['XERCESC_DOXDIR']
         env.AppendDoxref(doxref)
-
-        if prefix:
-            env.AppendUnique(CPPPATH=[os.path.join(prefix,'include'),])
 
         # If no prefix was set explicitly, and the 2.7 handling is enabled,
         # then look for special paths for the xerces-c27 package on Fedora.
