2ae1bb52d21c9a97d6f36265f5f7e1e35669db0c
Gordon Maclean <maclean@ucar.edu>
2012-12-17 19:48:06 +0000

    xmlrpc.py now does not execute rpm when trying to figure out where the xmlrpc++ softare is installed. This is so that this tool will work on Debian systems, such as a Raspberry PI.
    First try the xmlrpcpp pkg-config file, and if that doesn't exist,
    look in $OPT_PREFIX or /usr for lib{,64}/libxmlrpcpp.so.
    
    It now also assumes that all systems have been upgraded, so that
    the library is libxmlrpcpp.so and not the old libxmlrpc++.so.
    
    
    
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@8453 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/xmlrpc.py
+++ site_tools/xmlrpc.py
@@ -1,40 +1,57 @@
 import os
 
 def generate(env):
-    # See if the EOL xmlrpc++ RPM is installed.  If so, then up to 0.7-3:
+
+    # If xmlrpcpp pkg-config file exists, which is installed by the
+    # xmlrpc++ RPM, this should be all that's necessary
+    if (os.system('pkg-config --exists xmlrpcpp') == 0):
+        env.ParseConfig('pkg-config --cflags --libs xmlrpcpp')
+        return
+
+    # grope around for the XMLRPC C++ libraries.
+    # In xmlrpc++ RPMs up to 0.7-3:
     #    o headers are under /usr/include/xmlrpc++
-    #    o library is /usr/lib/libxmlrpc++.so
+    #    o library is /usr/lib{,64}/libxmlrpc++.so
     # For 0.7-4 and later:
     #    o headers are under /usr/include/xmlrpcpp
-    #    o library is /usr/lib/libxmlrpcpp.so
-    # The change was necessary because the new (and common) xmlrpc-c package
-    # contains a /usr/lib/libxmlrpc++.so that supports a different API than
-    # the xmlrpc++ package.
-    prefix = env.get('OPT_PREFIX')
-    if (os.system('rpm -V --quiet xmlrpc++') == 0):
-        # Check for libxmlrpcpp.so in both /usr/lib and /usr/lib64, so
-        # we're covered for both 32-bit and 64-bit installations.
-        if (os.path.exists("/usr/lib/libxmlrpcpp.so") or 
-            os.path.exists("/usr/lib64/libxmlrpcpp.so")):
-            env.Append(LIBS=['xmlrpcpp'])
-#            env.AppendUnique(DEPLOY_SHARED_LIBS=['xmlrpcpp'])
-            env.AppendUnique(CPPPATH = ['/usr/include/xmlrpcpp'])
-        else:
-            env.Append(LIBS=['xmlrpc++'])
-            env.AppendUnique(CPPPATH = ['/usr/include/xmlrpc++'])
-                         
-    # If no RPM, then assume headers are under OPT_PREFIX/include, and the
-    # library is in OPT_PREFIX/lib/libXmlRpc.a, but only if OPT_PREFIX has
-    # been enabled and set.
-    elif prefix:
-        cpppath = os.path.join(prefix, 'include')
-        libpath = os.path.join(prefix, 'lib')
-        if (os.path.exists(libpath + "/lib/libxmlrpcpp.so") or 
-            os.path.exists(libpath + "/lib64/libxmlrpcpp.so")):
-            env.AppendUnique(CPPPATH=[cppath])
-            env.AppendUnique(LIBPATH=[libpath])
-            env.Append(LIBS=['XmlRpc',])
+    #    o library is /usr/lib{,64}/libxmlrpcpp.so
+    # The change was necessary because the new (and common)
+    # xmlrpc-c-c++ package contains a /usr/lib/libxmlrpc++.so that
+    # supports a different API than the xmlrpc++ package.
 
+    # All systems should be updated to at least 0.7-4 by now,
+    # so don't check for /usr/lib{,64}/libxmlrpc++.so, even when
+    # the xmlrpc++ RPM is installed.
+
+    # Removed exec of rpm -q to check for the package, to be compatible
+    # with debian systems.
+
+    # if pkg-config file doesn't exist, look in the usual places
+    prefix = env.get('OPT_PREFIX')
+    if not prefix:
+        prefix = '/usr'
+
+    libpath = os.path.join(prefix, 'lib')
+    libpath64 = os.path.join(prefix, 'lib64')
+    if (os.path.exists(os.path.join(libpath,'libxmlrpcpp.so'))):
+        env.AppendUnique(CPPPATH = [os.path.join(prefix,'include','xmlrpcpp')])
+        if prefix != '/usr':
+            env.AppendUnique(LIBPATH=[libpath])
+        env.Append(LIBS=['xmlrpcpp'])
+
+    elif (os.path.exists(os.path.join(libpath64,'libxmlrpcpp.so'))):
+
+        env.AppendUnique(CPPPATH = [os.path.join(prefix,'include','xmlrpcpp')])
+        if prefix != '/usr':
+            env.AppendUnique(LIBPATH=[libpath64])
+        env.Append(LIBS=['xmlrpcpp'])
+
+    else:
+        # if built and installed from the original source, the library
+        # is called libXmlRpc.a
+        env.AppendUnique(CPPPATH=[os.path.join(prefix, 'include','XmlRpc')])
+        env.AppendUnique(LIBPATH=[os.path.join(prefix, 'lib')])
+        env.Append(LIBS=['XmlRpc'])
 
 def exists(env):
     return True
