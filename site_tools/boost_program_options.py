e3eeecf28cca4d75e06ebd85972e117195a167e2
Charles Martin <martinc@ucar.edu>
2013-02-04 15:50:13 +0000

    Handle the boost_program_options library handling for OSX in the same way that boost_date_time is handled. Note that both still allow Windows builds to correctly specify a suffix.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@8510 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/boost_program_options.py
+++ site_tools/boost_program_options.py
@@ -2,7 +2,10 @@ import os,os.path, sys
 import SCons
 
 def generate(env):
-    env.Append(LIBS=['boost_program_options',])
+    if env['PLATFORM'] != 'darwin':
+        env.Append (LIBS = ["boost_program_options"])
+    else:
+        env.Append (LIBS = ["boost_program_options-mt"])
     libpath = os.path.abspath(os.path.join(env['OPT_PREFIX'], 'lib'))
     env.AppendUnique(LIBPATH=[libpath])
 
