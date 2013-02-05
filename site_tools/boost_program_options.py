409af56cdd1fb3d3d050ced5bbadefe7629a1d50
Chris Burghart <burghart@ucar.edu>
2013-02-05 17:31:47 +0000

    As of Boost 1.50 (perhaps a bit before), libboost_program_options now explicitly depends on libboost_system, so add that library.
     I don't *think* adding the library here will break any older installations...
    
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@8514 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/boost_program_options.py
+++ site_tools/boost_program_options.py
@@ -3,9 +3,9 @@ import SCons
 
 def generate(env):
     if env['PLATFORM'] != 'darwin':
-        env.Append (LIBS = ["boost_program_options"])
+        env.Append (LIBS = ["boost_program_options", "boost_system"])
     else:
-        env.Append (LIBS = ["boost_program_options-mt"])
+        env.Append (LIBS = ["boost_program_options-mt", "boost_system-mt"])
     libpath = os.path.abspath(os.path.join(env['OPT_PREFIX'], 'lib'))
     env.AppendUnique(LIBPATH=[libpath])
 
