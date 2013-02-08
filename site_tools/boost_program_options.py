610426ccc8f631947b2ecf62cb513b059f2a1f1b
Charles Martin <martinc@ucar.edu>
2013-02-08 14:49:17 +0000

    RHEL5 does not have boost::system.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@8531 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/boost_program_options.py
+++ site_tools/boost_program_options.py
@@ -1,11 +1,36 @@
 import os,os.path, sys
 import SCons
 
+def has_boost_system(env, libname):
+    """See if boost::system is available. It was introduced
+    in Boost v1.35, and so is not found on RHEL5 systems.
+    We try to compile a program which references boost::system::error_code.
+    
+    Returns: True if found, False if not.
+    """
+    
+    # Define the test program
+    header = 'boost/system/error_code.hpp'
+    test_src = 'boost::system::error_code code;'
+
+    # Try to build it
+    conf = env.Clone(LIBS=[]).Configure()
+    has_it = conf.CheckLibWithHeader(libname, header, 'CXX', test_src)
+    conf.Finish()
+
+    return has_it
+
 def generate(env):
+
     if env['PLATFORM'] != 'darwin':
-        env.Append (LIBS = ["boost_program_options", "boost_system"])
+        env.Append (LIBS = ["boost_program_options"])
+        if has_boost_system(env, "boost_system"):
+            env.Append (LIBS = ["boost_system"])
     else:
-        env.Append (LIBS = ["boost_program_options-mt", "boost_system-mt"])
+        env.Append (LIBS = ["boost_program_options-mt"])
+        if has_boost_system(env, "boost_system-mt"):
+            env.Append (LIBS = ["boost_system-mt"])
+                
     libpath = os.path.abspath(os.path.join(env['OPT_PREFIX'], 'lib'))
     env.AppendUnique(LIBPATH=[libpath])
 
