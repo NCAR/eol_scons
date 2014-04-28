096a22e848d96e329342ab86e714b08c93176d0d
Chris Burghart <burghart@ucar.edu>
2014-04-28 20:47:53 +0000

    Added a once-per-build warning to encourage moving from the boost_mutex tool to boost_thread.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@8975 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/boost_mutex.py
+++ site_tools/boost_mutex.py
@@ -5,9 +5,15 @@ import SCons
 # Use this tool for all Boost mutex classes (I'm using it for 
 # boost::recursive_mutex). The mutex classes always require libpthread,
 # and depending on Boost version, may also require libboost_thread.
+warned = False
+
 def generate(env):
     # We now just use the boost_thread tool to get boost::mutex support.
     env.Require(['boost_thread'])
+    global warned
+    if not warned:
+        print '\nWARNING: The boost_mutex SCons tool is deprecated; switch to boost_thread.\n'
+        warned = True
 
 def exists(env):
     return True
