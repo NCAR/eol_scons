8337f8ba7667013089d47260c3b650168711a16a
Chris Burghart <burghart@ucar.edu>
2014-04-28 20:40:38 +0000

    Added new SCons tool boost_thread, adapted from the old boost_mutex tool, with more exhaustive testing to find the appropriate library or libraries needed for linking. The boost_mutex tool now just requires the new boost_thread tool.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@8974 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/boost_mutex.py
+++ site_tools/boost_mutex.py
@@ -1,46 +1,13 @@
-import os,os.path, sys
 import SCons
 
-
+# As of 4/2014, this tool is deprecated; use the boost_thread tool instead.
+#
 # Use this tool for all Boost mutex classes (I'm using it for 
 # boost::recursive_mutex). The mutex classes always require libpthread,
 # and depending on Boost version, may also require libboost_thread.
 def generate(env):
-    # For older versions of Boost mutexes, we need to link with both
-    # boost_thread and pthread libraries. For later versions, we only
-    # need pthread. Use Environment.Configure to test which way will work
-    # on this system.
-    # Try, in order,
-    #       -lpthread
-    #       -lboost_thread-mt -lpthread
-    #       -lboost_thread -lpthread
-    # to see if any combination will work. If not, complain and quit.
-    clone = env.Clone()
-    clone.Replace(LIBS=[])
-    # Don't go any further if they're cleaning or asked for help
-    if env.GetOption('clean') or env.GetOption('help'):
-        return
-
-    conf = clone.Configure()
-    # Disable SConf display of messages like "Checking for <x> in <y>..."
-    origPrintState = SCons.SConf.progress_display.print_it
-    SCons.SConf.progress_display.print_it = False
-    
-    header = 'boost/thread/mutex.hpp'
-    test_src = 'boost::mutex m; boost::mutex::scoped_lock guard(m);'
-    if (not conf.CheckLibWithHeader('pthread', header, 'CXX', test_src) and
-        not conf.CheckLibWithHeader(['boost_thread-mt', 'pthread'], header, 'CXX', test_src) and
-        not conf.CheckLibWithHeader(['boost_thread', 'pthread'], header, 'CXX', test_src)):
-            msg = "Failed to link to boost::mutex both with and without"
-            msg += " libboost_thread.  Check config.log."
-            raise SCons.Errors.StopError, msg
-
-    conf.Finish()
-        
-    env.Append(LIBS=clone['LIBS'])
-    
-    # Restore SConf's print state
-    SCons.SConf.progress_display.print_it = origPrintState
+    # We now just use the boost_thread tool to get boost::mutex support.
+    env.Require(['boost_thread'])
 
 def exists(env):
     return True
