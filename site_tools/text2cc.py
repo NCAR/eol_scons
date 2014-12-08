d90d9d631fa6eb2675b40240c938fb224c10d094
Gary Granger <granger@ucar.edu>
2014-12-08 22:43:13 +0000

    add a tool which embeds text files in C source code, borrowed from mapr config for embedding the XML DTD
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@9135 da89c675-a4d7-0310-8a15-9e7546503515

---
+++ site_tools/text2cc.py
@@ -0,0 +1,16 @@
+# -*- python -*-
+#
+# SConscript tool which adds a pseudo-builder to embed a text file in C++
+# code.
+
+_text2cc = r"""cat $SOURCE | (echo 'const char* $TEXT_DATA_VARIABLE_NAME = ' && sed -e 's/\"/\\\"/g' -e 's/^/\"/g' -e 's/$$/\\n"/' && echo ';') > $TARGET"""
+
+
+def _EmbedTextCC(env, target, source, variable):
+    env.Command(target, source, _text2cc, TEXT_DATA_VARIABLE_NAME = variable)
+
+def generate(env):
+    env.AddMethod(_EmbedTextCC, "EmbedTextCC")
+
+def exists(env):
+    return True
