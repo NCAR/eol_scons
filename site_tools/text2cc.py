3f487835acdad7edb6d4a9e91e6137b4c7a0fd79
Gary Granger <granger@ucar.edu>
2014-12-19 22:34:46 +0000

    Include DO NOT EDIT warning at the top of the generated source file.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@9143 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/text2cc.py
+++ site_tools/text2cc.py
@@ -3,7 +3,7 @@
 # SConscript tool which adds a pseudo-builder to embed a text file in C++
 # code.
 
-_text2cc = r"""cat $SOURCE | (echo 'const char* $TEXT_DATA_VARIABLE_NAME = ' && sed -e 's/\"/\\\"/g' -e 's/^/\"/g' -e 's/$$/\\n"/' && echo ';') > $TARGET"""
+_text2cc = r"""cat $SOURCE | (echo '/***** DO NOT EDIT *****/' && echo 'const char* $TEXT_DATA_VARIABLE_NAME = ' && sed -e 's/\"/\\\"/g' -e 's/^/\"/g' -e 's/$$/\\n"/' && echo ';') > $TARGET"""
 
 
 def _EmbedTextCC(env, target, source, variable):
