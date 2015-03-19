fe3af10ae2b2b9685ef5a90b880b5993c499c26d
Gary Granger <granger@ucar.edu>
2015-03-19 22:11:24 +0000

    check for udunits2 if udunits not available
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@9284 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/udunits.py
+++ site_tools/udunits.py
@@ -2,8 +2,17 @@ import os
 
 
 def generate(env):
-    env.Append(LIBS=['udunits'])
-
+    # Look for udunits, and if not found look for udunits2.  We need a
+    # clean environment, though, else other LIBS can cause link problems.
+    ckenv = env.Clone()
+    ckenv['LIBS'] = []
+    conf = ckenv.Configure()
+    if conf.CheckLib('udunits'):
+        env.Append(LIBS=['udunits'])
+    elif conf.CheckLib('udunits2'):
+        env.Append(LIBS=['udunits2'])
+        env.Append(CPPPATH=['/usr/include/udunits2'])
+    ckenv = conf.Finish()
 
 def exists(env):
     return True
