5c27193ab567b2d71e650295dbf06bbd392087bd
Gary Granger <granger@ucar.edu>
2015-04-03 15:21:19 +0000

    On linux at least, I think it is enough to have quarter require the coin tool, since the quarter built against that coin also installs into the same places.
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@9304 da89c675-a4d7-0310-8a15-9e7546503515

--- site_tools/quarter.py
+++ site_tools/quarter.py
@@ -3,12 +3,16 @@ import sys
 
 def generate(env):
     env.Require('qt4')
+    env.Require('coin')
     
     if sys.platform in ['linux', 'linux2']: 
-        env.Append(CPPPATH=['/usr/local/include'])
-        env.Append(LIBPATH=['/usr/local'])
-        env.AppendUnique(LIBS=['Quarter', 'Coin'])
-        
+        # There is no need to set specific include or library paths here.
+        # It is enough to require the coin tool above, since Quarter
+        # installs into the same location.  When built from source, both
+        # install into the default configure prefix of /usr/local/.  When
+        # installed as an RPM, Quarter headers are in /usr/include/Coin2
+        # and the Quarter library is in the same place as the Coin library.
+        env.AppendUnique(LIBS=['Quarter'])
 
     if sys.platform == 'darwin':
         qtframes = ['QtGui', 'QtCore', 'QtOpenGL']
