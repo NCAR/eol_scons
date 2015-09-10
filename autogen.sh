93152a88d889b1478b913d94e9800921af891e93
Gordon Maclean <maclean@ucar.edu>
2015-09-10 03:41:22 +0000

    more autoconfiscation, getting closer
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@9399 da89c675-a4d7-0310-8a15-9e7546503515

--- autogen.sh
+++ autogen.sh
@@ -3,17 +3,18 @@
 set -e
 
 echo "Adding libtools."
-libtoolize --force --automake --ltdl
+libtoolize --force --automake
 
 echo "Building macros."
-ACLOCAL_FLAGS="-I config --output=config/aclocal.m4 $ACLOCAL_FLAGS"
+# ACLOCAL_FLAGS="-I config --output=config/aclocal.m4 $ACLOCAL_FLAGS"
+ACLOCAL_FLAGS="-I config $ACLOCAL_FLAGS"
 aclocal $ACLOCAL_FLAGS
 
 echo "Building config header."
 autoheader
 
 echo "Building makefiles."
-automake --add-missing --foreign
+automake -f --add-missing --foreign
 
 echo "Building configure."
 autoconf
