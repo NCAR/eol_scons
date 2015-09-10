2cc33277d0e3e2e2a0dd405ecbd35a18f374f7a9
Gordon Maclean <maclean@ucar.edu>
2015-09-10 03:41:24 +0000

    move ltmain.sh to config if libtoolize doesn't
    git-svn-id: http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons@9404 da89c675-a4d7-0310-8a15-9e7546503515

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,12 @@
 set -ex
 
 libtoolize --force --automake
+# If libtoolize puts ltmain.sh in this directory, for what reason I
+# cannot figure out, then move it to config
+if [ -f ltmain.sh ]; then
+    rm -f config/ltmain.sh
+    mv ltmain.sh config
+fi
 aclocal -I config
 autoheader
 automake -f --add-missing --foreign
