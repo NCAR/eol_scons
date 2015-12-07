fc52edac7a5bb530f48233fec55a2ca795c9403f
Gordon Maclean <maclean@users.noreply.github.com>
2015-12-06 21:14:56 -0700

    Simplify git_svn_mirror.sh script

--- scripts/mirror_git_to_svn/git_svn_mirror.sh
+++ scripts/mirror_git_to_svn/git_svn_mirror.sh
@@ -1,59 +1,38 @@
 #!/bin/sh
 
-if ! [ $JENKINS_HOME ]; then
-    set -x
-    set -e
-    cd /tmp
+svnurl=http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons
 
-
-    if [ ! -d eol_scons ]; then
-        git clone http://github.com/ncareol/eol_scons eol_scons
-    fi
-
-    cd eol_scons
-
-fi
-
-# url = file:///scr/tmp/maclean/svn_mirror/eol_svn_repo/common/trunk/site_scons
 if ! [ -f .git/config ] || ! grep -F -q svn-remote .git/config; then
     cat << EOD >> .git/config
 [svn-remote "svn"]
-    url = http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons
+    url = $svnurl
     fetch = :refs/remotes/git-svn
 EOD
 fi
 
-# takes a long time the first time it is run
+# fetch from above svn-remote named "svn"
+# takes a long time the first time it is run from a large repo
 git svn fetch svn 
 
-# check if branch "svn" exists
+# create master branch if needed
+git show-ref --verify --quiet refs/heads/master || git branch master origin/master
+
+# create svn branch if it doesn't exist, tracking git-svn remote
+# must be done after above git svn fetch
 git show-ref --verify --quiet refs/heads/svn || git branch svn git-svn
-# git checkout svn
 
-# get origin master, with original SHA1
-git show-ref --verify --quiet refs/heads/tmp-master || git branch tmp-master origin/master
-git checkout tmp-master
-git pull origin master
-
-# merge in new master, then rebase on to SVN
-git checkout master
-git pull origin master
-git merge --ff-only tmp-master
-
-# If original fetch is done on "-r HEAD" then this step
-# takes a long time. No getting around it.
-git rebase svn
+# master becomes a series of commits after HEAD of svn
+# takes a long time the first time it is run on a large repo
+git rebase svn master
 
 git checkout svn
+
+# fast-forward merge the new commits on master to svn
 git merge --ff-only master
 
+# push new commits to subversion
 git svn dcommit
 
-git checkout master
-# rebase master back to original SHA1
-git rebase tmp-master
-
-git branch -D tmp-master
-# git branch -D svn
-
+# set master back to origin/master
+git rebase origin/master master
 
