06c5cd573bea4396864b7f5c88645d279c840199
Gordon Maclean <maclean@users.noreply.github.com>
2015-12-09 11:40:09 -0700

    Suppress "detached head" warning on git checkout

--- scripts/mirror_git_to_svn/git_svn_mirror.sh
+++ scripts/mirror_git_to_svn/git_svn_mirror.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
 svnurl=http://svn.eol.ucar.edu/svn/eol/common/trunk/site_scons
-giturl=https://github.com/ncareol/eol_scons.git
 
 # For testing outside of jenkins
 if ! [ $JENKINS_HOME ]; then
 
     repo=eol_scons
-    JENKINS_HOME=/tmp/${repo}_jenkins
 
+    JENKINS_HOME=/tmp/${repo}_jenkins
     [ -d $JENKINS_HOME ] || mkdir $JENKINS_HOME
     cd $JENKINS_HOME
+
+    giturl=https://github.com/ncareol/${repo}.git
 
     # emulate how jenkins sets up the working tree
     # jenkins works in a "detached head" state, with no current
     # branch, doing a checkout on a specific commit
 
     git rev-parse --is-inside-work-tree || git init
-
     git config remote.origin.url $giturl
-
     git -c core.askpass=true fetch --tags --progress $giturl '+refs/heads/*:refs/remotes/origin/*'
 
     GIT_COMMIT=$(git rev-parse origin/master^{commit})
@@ -46,7 +45,7 @@ git show-ref --verify --quiet refs/heads/tmp-master && git branch -D tmp-master
 git branch tmp-master $GIT_COMMIT
 git checkout tmp-master
 
-# rebase tmp-master so becomes a series of commits after HEAD of svn
+# rebase tmp-master so it becomes a series of commits after HEAD of svn
 # takes a long time the first time it is run on a large repo
 git rebase svn
 
@@ -57,8 +56,11 @@ git merge --ff-only tmp-master
 # push new commits to subversion
 git svn dcommit
 
-# reset working tree
-git checkout -f $GIT_COMMIT
+# reset working tree, quietly, to suppress the "detached head" warning
+git checkout -f -q $GIT_COMMIT
 
+# delete temporary branch
 git branch -D tmp-master
+
+git status
 
