e04175659c1ef447cf5ca8181aa625e87b1ebb81
Gary Granger <granger@ucar.edu>
2015-12-11 10:27:32 -0700

    tweak the download semantics and update documentation

--- eol_scons/tools/datafilecache.py
+++ eol_scons/tools/datafilecache.py
@@ -1,17 +1,61 @@
 """
-A SCons tool which adds a DataFileCache instance to an environment and
-integrates with SCons builders to download data files and create
+A SCons tool which adds a DataFileCache instance to an Environment and
+integrates it with SCons builders to download data files and create
 dependencies on the cached copies.
+
+These are the methods added to the Environment:
+
+  DataFileCache(): Return an instance of DataFileCache attached to the
+  environment instance, creating it first if necessary.
+
+  DownloadDataFile(): Register a cache file by its relative path, and
+  return a SCons Node for the local cache path to that file.  The node is
+  the target of a builder which runs DataFileCache.download() to
+  synchronize the local file.
+
+If a cached data file is a dependency of a scons target being built, and it
+does not already exist in the cache, then the file will be downloaded with
+rsync from the remote specifier.  Once a file has been downloaded to the
+cache, it will not be synchronized with the remote source again.  The cache
+can be updated using the 'datasync' alias described below.
+
+This tool adds a scons variable called 'download', with one of three
+settings: force, auto, and off.  The default setting is 'auto'.
+
+   force: Files will be explicitly synchronized whenever required for a
+          dependency, even if they already exist in the cache.
+
+   off: Files will never be synchronized.  This can be helpful if the local
+        cache is already current, but the scons dependency cache has not
+        been updated.  For example, to prevent downloads in a fresh working
+        tree or after cleaning the scons cache files, run 'scons
+        download=off <targets>'.  The local cache files will be used as is
+        without attempting to synchronizing them.  If a local cache file is
+        needed for a dependency but does not exist, and 'download' is
+        disabled, then the build will fail.
+
+   auto: File synchronization follows scons dependency tracking.  If a file
+         already exists locally and the scons dependency info is
+         up-to-date, then no synchronization is run.  If the dependency
+         info is not up-to-date, then the file will be synchronized even if
+         it already exists locally, because scons has no record that the
+         target file is current.
+
+Each cached data file is added to the 'datasync' alias.  Run 'scons
+datasync' to build all the cached data file targets, and set the 'download' option
+to choose whether the files should be synchronized.  For example, this will update 
+all the data files in the cache:
+
+   scons download=force datasync
+
+This command will just check that all the files exist in the cache and fail
+if any do not:
+
+   scons download=off datasync
 """
 
 import SCons
-from SCons.Variables import BoolVariable
-
-def _sync_cache(target, source, env):
-    dfcache = env.DataFileCache()
-    if dfcache.sync():
-        return None
-    raise SCons.Errors.StopError("datasync failed.")
+from SCons.Variables import EnumVariable
 
 def _sync_file(target, source, env):
     dfcache = env.DataFileCache()
@@ -22,19 +66,34 @@ def _sync_file(target, source, env):
         msg = "datasync failed, download disabled."
     raise SCons.Errors.StopError(msg)
 
+def _sync_file_message(target, source, env):
+    return "Downloading %s to %s:" % (str(source[0]), str(target[0]))
+
 def _download_data_file(env, filepath):
     # Create a scons builder which downloads the source file into the cache.
     dfcache = env.DataFileCache()
-    dfcache.enableDownload(env['download'])
+    dfcache.enableDownload(env.get('download', 'auto') in ['auto', 'force'])
+    syncfile = env.Action(_sync_file, _sync_file_message)
     target = env.Command(dfcache.getFile(filepath),
-                         env.Value(filepath), _sync_file)
+                         env.Value(filepath), syncfile)
+    if env.get('download', 'auto') == 'force':
+        env.AlwaysBuild(target)
     # Do not allow scons to erase the data file before re-synchronizing it.
     env.Precious(target)
     print("created command builder to download %s to %s" %
           (filepath, target[0].abspath))
-    # return target[0].abspath
-    # Return just the single node rather than the list that a builder
-    # would actually return, so it can be substitued easily for a file path.
+    # Add the target to the datasync alias, so one alias can be used to
+    # update all the cache files and also the scons dependencies cache.
+    # However, the syncs do not happen if the file already appears updated,
+    # so we have to kludge it a little bit.  If datasync is one of the
+    # targets to be built, then the datafile targets themselves need to be
+    # forced with AlwaysBuild().  Rather than force it here, rely on the
+    # download setting above to force downloads.
+    env.AlwaysBuild(env.Alias('datasync', target))
+    if False and 'datasync' in SCons.Script.BUILD_TARGETS:
+        env.AlwaysBuild(target)
+    # Return just the single node rather than the list that a builder would
+    # actually return, so it can be substitued easily for a file path.
     return target[0]
 
 def _get_cache_instance(env):
@@ -63,19 +122,14 @@ def generate(env):
     global _options
     if not _options:
         _options = env.GlobalVariables()
-        _options.Add(BoolVariable('download',
-                                  "Enable or disable downloading of data files "
-                                  "to the local data file cache.", True))
+        _options.Add(EnumVariable('download',
+                                  "Set whether data file downloading is forced, "
+                                  "automatic, or completely disabled.", 'auto',
+                                  allowed_values=('force', 'auto', 'off'),
+                                  ignorecase=2))
     _options.Update(env)
     env.AddMethod(_get_cache_instance, "DataFileCache")
     env.AddMethod(_download_data_file, "DownloadDataFile")
-    # Automatically provide a datasync alias for now, but this may be
-    # better off deferred to an explicit request to create the alias.  If
-    # this is deferred until after all the cache files are registered, then
-    # the targets of this alias can be the actual cached files built by the
-    # download builders, and the separate _sync_cache function would be
-    # unnecessary.
-    env.AlwaysBuild(env.Alias('datasync', None, _sync_cache))
 
 def exists(env):
     return True
