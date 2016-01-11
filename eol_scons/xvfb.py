0d2f7ffd98abc5b0986acaaf665d493b8750c25e
Gary Granger <granger@ucar.edu>
2016-01-11 13:40:33 -0700

    move Xvfb into its own module for use separately from scons

---
+++ eol_scons/xvfb.py
@@ -0,0 +1,72 @@
+"Wrap the Xvfb process to provide a headless X server for scripts."
+
+import os
+import subprocess as sp
+import select
+
+class Xvfb(object):
+
+    """
+    Wrap a Xvfb subprocess and provide methods to start and stop it.
+    """
+
+    def __init__(self):
+        self.saved_display = None
+        self.display = None
+        self.proc = None
+        self.dpipe = None
+        self.displayfd = None
+
+    def start(self):
+        dpipe = os.pipe()
+        cmd = ['Xvfb', '-displayfd', str(dpipe[1])]
+        # The -displayfd option causes Xvfb to look for an available
+        # display number, but it writes error messages for each failed
+        # attempt to open a display.  It would be nice to create a pipe for
+        # stderr to filter out the error messages, but that gets
+        # complicated because we would need to keep reading from that pipe
+        # after starting Xvfb or else risk blocking the Xvfb process.
+        # Likely there would not be any output once the display number is
+        # written back over the pipe, but that is not guaranteed.  Instead
+        # it works to use the shell to pipe and filter with grep.
+        cmd = "Xvfb -displayfd %d |& grep -v 'SocketCreateListener() failed' "
+        cmd += " | grep -v 'server already running'"
+        cmd = cmd % (dpipe[1])
+        print(cmd)
+        self.proc = sp.Popen(cmd, close_fds=False, stdout=None, stderr=None,
+                             shell=True)
+        rfds = [dpipe[0]]
+        displaybuf = ""
+        # Since I cannot figure out how to open the pipe fds nonblocking,
+        # resort to using select and reading one character at a time.  This
+        # code must handle two cases: the display fd has been written
+        # successfully to the pipe, or else the Xvfb process has exited and
+        # will never write to the pipe.
+        while '\n' not in displaybuf:
+            (readable, writable, xable) = select.select(rfds, [], [], 1)
+            if dpipe[0] in rfds:
+                displaybuf = displaybuf + os.read(dpipe[0], 1)
+            xcode = self.proc.poll()
+            if xcode is not None:
+                print("*** Xvfb exited with return code %d ***" % (xcode))
+                return None
+
+        self.display = ':'+str(int(displaybuf))
+        os.close(dpipe[1])
+        os.close(dpipe[0])
+        self.saved_display = os.environ.get('DISPLAY')
+        os.environ['DISPLAY'] = self.display
+        return self
+
+    def stop(self):
+        if self.proc is None:
+            return
+        self.proc.kill()
+        self.proc.wait()
+        self.proc = None
+        self.display = None
+        if self.saved_display is not None:
+            os.environ['DISPLAY'] = self.saved_display
+        else:
+            del os.environ['DISPLAY']
+
