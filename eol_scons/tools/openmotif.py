991795d505e070e51b42defe88c6b1b50dc7ff9e
Chris Webster <cjw@ucar.edu>
2017-04-05 08:34:20 -0600

    Correct misspelled LIBPATH. Remove lib Xext, doesn't seem to be required on macos or centos7.

--- eol_scons/tools/openmotif.py
+++ eol_scons/tools/openmotif.py
@@ -3,10 +3,10 @@
 import os, os.path
 
 def generate(env):
-  env.Append(LIBS=['Xm','Xt','X11','Xext'])
+  env.Append(LIBS=['Xm','Xt','X11'])
   if env['PLATFORM'] == 'darwin':
     env.Append(CPPPATH=['/opt/X11/include'])
-    env.Append(LIBPPATH=['/opt/X11/lib'])
+    env.Append(LIBPATH=['/opt/X11/lib'])
 
 def exists(env):
     return True
