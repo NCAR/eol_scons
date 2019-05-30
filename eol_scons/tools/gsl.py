03d6be38dc3127b27662673e12fae9d789f1b7e3
Chris Webster <cjw@ucar.edu>
2019-05-30 11:10:03 -0600

    Add comment to code for previous commit.

--- eol_scons/tools/gsl.py
+++ eol_scons/tools/gsl.py
@@ -4,6 +4,8 @@ import os, os.path
 
 def generate(env):
   env.Append(LIBS=['gsl','gslcblas'])
+  # On msys the math functions come in via libmsvcrt.a, and there will be
+  # multiple definitions if add -lm.
   if env['PLATFORM'] != 'msys':
     env.Append(LIBS=['m'])
 
