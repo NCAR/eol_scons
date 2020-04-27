67b4d2e34cafab615b56c7dee0d620a0e5bbf0f9
Gary Granger <granger@ucar.edu>
2020-04-26 18:04:07 -0600

    no point trying to build against source when the location is unknown
    Since the path to the raf source inside a source tree is not known relative
    to the environment to which the tool is being applied, there is no point in
    trying to add the right include path.  Likewise for the library.  This
    reverts this tool to working only for the installed raf, by adding raf to
    the LIBS.  The in-source raf tool will be found using a tool_raf.py.

--- eol_scons/tools/raf.py
+++ eol_scons/tools/raf.py
@@ -1,8 +1,13 @@
 # -*- python -*-
 
+# This tool works for source trees which do not contain the raf library
+# source but still need to link to it.  If the raf source is in the source
+# tree, then tool_raf.py tool will be loaded instead of this one.
+
+# This tool expects that CPPPATH and LIBPATH have already been set
+# appropriately using tools like prefixoptions or jlocal.
+
 def generate(env):
-    env.Append(CPPPATH=['.'])
-    env.Append(LIBPATH=['#/raf'])
     env.AppendLibrary('raf')
 
 def exists(env):
