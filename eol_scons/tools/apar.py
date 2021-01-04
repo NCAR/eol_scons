3cd77ea2c165505d199302612a493ee403cf7cfc
Chris Burghart <burghart@ucar.edu>
2021-01-04 23:15:15 +0000

    Add tool for APAR's AparData lib

---
+++ eol_scons/tools/apar.py
@@ -0,0 +1,84 @@
+# -*- python -*-
+
+import os, os.path
+import string
+import SCons
+
+# APAR requires LROSE
+dep_tools=['lrose']
+
+_apar_source_file = """
+#include <AparData/AparTsInfo.hh>
+int main(int argc, char **argv)
+{
+    AparTsInfo info;
+    info.setDebug(AparTsDebug_t::OFF);
+    return 0;
+}
+"""
+
+def CheckApar(context):
+    context.Message('Checking for APAR linking...')
+    result = context.TryLink(_apar_source_file, '.cpp')
+    context.Result(result)
+    return result
+
+_settings = {}
+
+aparLibs = ['AparData']
+
+def _calculate_settings(env, settings):
+    # Look for LROSE under $LROSE_INSTALL_DIR, /usr/local/lrose,
+    # or /opt/local/lrose
+    prefix = None
+    if ('APAR_INSTALL_DIR' in os.environ):
+        paths = [os.environ['APAR_INSTALL_DIR']]
+    else:
+        paths = []
+    paths += ['/usr/local/apar', '~/apar']
+    for path in paths:
+        if os.path.isdir(path):
+            prefix = path
+            break
+    if not prefix:
+        msg = "Unable to find APAR. No directory in [%s] exists." % (', '.join(paths))
+        raise SCons.Errors.StopError(msg)
+    else:
+        print("Using APAR directory", prefix)
+    
+    # Libs will be in <prefix>/lib
+    libdir = os.path.join(prefix, 'lib')
+    settings['LIBDIR'] = libdir
+    
+    # Headers will be in <prefix>/include
+    headerdir = os.path.join(prefix, 'include')
+    settings['CPPPATH'] = [ headerdir ]
+
+    settings['LIBS'] = aparLibs
+
+    if env.GetOption('clean') or env.GetOption('help'):
+        return
+
+    clone = env.Clone()
+    clone.Replace(LIBS=aparLibs)
+    clone.Require(dep_tools)
+    clone.AppendUnique(CPPPATH=settings['CPPPATH'])
+    clone.AppendUnique(LIBPATH=[settings['LIBDIR']])
+    conf = clone.Configure(custom_tests = { "CheckApar" : CheckApar })
+    if not conf.CheckApar():
+        msg = "Failed to link to APAR. Check config.log."
+        raise SCons.Errors.StopError(msg)
+    conf.Finish()
+
+def generate(env):
+    if not _settings:
+        _calculate_settings(env, _settings)
+    env.Require(dep_tools)
+    env.AppendUnique(CPPPATH=_settings['CPPPATH'])
+    env.Append(LIBS=_settings['LIBS'])
+    env.AppendUnique(LIBPATH=[_settings['LIBDIR']])
+    env.AppendUnique(LINKFLAGS = ['-Wl,-rpath,' + _settings['LIBDIR']])
+
+def exists(env):
+    return True
+
