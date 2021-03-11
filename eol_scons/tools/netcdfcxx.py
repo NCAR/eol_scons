fb6bb098c2cfacf46e05060ef1373cd5fa37d9f2
Gary Granger <granger@ucar.edu>
2021-03-11 13:07:23 -0700

    oops, same fix for legacy netcdf c++
    and update comments in both tools to be more consistent

--- eol_scons/tools/netcdfcxx.py
+++ eol_scons/tools/netcdfcxx.py
@@ -3,9 +3,9 @@ def generate(env):
     """
     Tool for the legacy netcdf-c++ library.
     """
-    # env.AppendUnique(DEPLOY_SHARED_LIBS=['netcdf_cxx'])
-    env.AppendUnique(LIBS=['netcdf_c++'])
+    env.Append(LIBS=['netcdf_c++'])
     env.Require('netcdf')
+
 
 def exists(env):
     return True
--- eol_scons/tools/netcdfcxx4.py
+++ eol_scons/tools/netcdfcxx4.py
@@ -3,7 +3,6 @@ def generate(env):
     """
     Tool for the netcdf-cxx4 library which replaces the legacy C++ API.
     """
-    # env.AppendUnique(DEPLOY_SHARED_LIBS=['netcdf_cxx4'])
     env.Append(LIBS=['netcdf_c++4'])
     env.Require('netcdf')
 
