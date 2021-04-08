97bc29dd533cdbbfd1a7beae8c3b9f2fa32db2fb
Gary Granger <granger@ucar.edu>
2021-04-08 12:39:21 -0600

    remove get_svnversion, fix flake8 warnings
    Turns out get_svnversion is not used anywhere, so remove it.
    Run utils.py directly to test get_cxxversion output.

--- eol_scons/utils.py
+++ eol_scons/utils.py
@@ -1,32 +1,27 @@
 # -*- python -*-
 # Copyright 2007 UCAR, NCAR, All Rights Reserved
 
-import os
 import re
 import subprocess
 
-def get_svnversion(env):
-    """
-    Return value reported by svnversion on top directory.
-    """
-    svn_path = env.WhereIs('svnversion', os.environ['PATH'])
-    revision = "Unknown"
-
-    if svn_path != None:
-        rev = os.popen('svnversion ' + env.Dir('#.').abspath).readline().strip()
-        if rev != "":
-            revision = rev
-    return revision
 
 def get_cxxversion(env):
     try:
         # do g++ --version, grab 3rd field for CXXVERSION
-        revline = subprocess.Popen([env['CXX'],'--version'],
-                                   env={'PATH':env['ENV']['PATH']},
-                                   stdout=subprocess.PIPE,universal_newlines=True).stdout.readline()
-        rev = re.split('\s+',revline)[2]
+        revline = subprocess.Popen([env['CXX'], '--version'],
+                                   env={'PATH': env['ENV']['PATH']},
+                                   universal_newlines=True,
+                                   stdout=subprocess.PIPE).stdout.readline()
+        rev = re.split(r'\s+', revline)[2]
         return rev
     except OSError as xxx_todo_changeme:
-        (errno,strerror) = xxx_todo_changeme.args
-        print("Error: %s: %s" %(env['CXX'],strerror))
+        (errno, strerror) = xxx_todo_changeme.args
+        print("Error: %s: %s" % (env['CXX'], strerror))
         return None
+
+
+if __name__ == "__main__":
+    import eol_scons
+    from SCons.Script import Environment
+    env = Environment(tools=['default'])
+    print("CXX version: %s" % (get_cxxversion(env)))
