4412f1a646ed538134a93d15a80a23bd29a958c5
Gary Granger <granger@ucar.edu>
2021-04-13 15:26:31 -0600

    set python-config path with a variable
    For systems with both python3-config and python-config on the path,
    add PYTHON_CONFIG variable to pick one.  Also, if python-config
    does not report a python shared library to link to, or if it does,
    no libboost_python can be found which depends on that library,
    then just take whatever is found first.  Anaconda installs may
    not report a python library in python-config --libs, and the
    boost_python library may not list a python library as a dynamic
    dependency.  So really that leaves not much choice.  And even
    though a compile may seem to work against anaconda3 python,
    loading the module still fails with missing symbol errors, and that
    is still a mystery.

--- eol_scons/tools/boost_python.py
+++ eol_scons/tools/boost_python.py
@@ -21,53 +21,124 @@ libboost_python*.so which depends on that same python library.
 import re
 
 import SCons
+from SCons.Script import FindPathDirs
 
 from eol_scons.ldd import ldd
 from eol_scons.parseconfig import RunConfig
+
+_variables = None
+
 
 def _find_boost(env, libpython):
     """
     Find all the boost_python libraries, then pick out the one which
     depends on the python library passed in libpython.  There's a big
     assumption here that the first one found will be correct.  Really this
-    should check in order the standard library paths and then LIBPATH.
+    should check in order the standard library paths and then LIBPATH.  The
+    point is that if the python library has a version in it's name, like
+    libpython3.6m, then this will find the boost_python library file which
+    has that python library name as a dynamic dependency.  Otherwise,
+    default to the first one found, in case the LIBPATH has been set to
+    find the compatible libraries first.  This can still return None if
+    there are no files in the library search path matching
+    libboost_python*.
     """
-    libpaths = env.Glob("/usr/lib*/libboost_python*.so")
+    libdirs = list(FindPathDirs('LIBPATH')(env))
+    libdirs.extend(env.Dir(['/usr/lib64', '/usr/lib']))
+    libpaths = []
+    for libdir in libdirs:
+        libpaths.extend(env.Glob(libdir.abspath + "/libboost_python*.so"))
+    env.LogDebug("scanning boost_python libs: %s" %
+                 (",".join([str(lp) for lp in libpaths])))
+    firstlib = None
     for lib in libpaths:
+        if firstlib is None:
+            firstlib = lib
         deps = ldd(lib, env)
-        plibs = {k:v for k,v in deps.items() if 'python' in k}
-        for k,v in plibs.items():
+        plibs = {k: v for k, v in deps.items() if 'python' in k}
+        for k, v in plibs.items():
             if k == libpython:
                 return lib
+    # If we get here, then no boost_python library was found with the right
+    # python dynamic dependency, so just return the first one found, on the
+    # assumption it's the right one because it appears first in the
+    # LIBPATH.  It might make sense to confirm the library is in the same
+    # directory as the first python library, but not until proven
+    # necessary.
+    env.LogDebug("could not find boost_python library which depends on %s, "
+                 "using first boost_python found: %s" % (libpython, firstlib))
+    return firstlib
 
 
 def generate(env):
+    require_plib = False
+    global _variables
+    if _variables is None:
+        _variables = env.GlobalVariables()
+        _variables.Add('PYTHON_CONFIG', """
+Path to the python config script, such as python-config or python3-config.
+The script is called to add the include and link flags for compiling against
+that python library.  python-config is the default.""".strip())
 
-    pconfig = env.WhereIs("python3-config") or env.WhereIs("python-config")
+    _variables.Update(env)
+    pconfig = env.get('PYTHON_CONFIG', 'python-config')
+    # WhereIs() will make sure an absolute path exists or else a relative
+    # path is on the PATH.
+    pconfig = env.WhereIs(pconfig)
     if not pconfig:
-        msg = "boost_python requires python3-config or python-config."
+        msg = str("boost_python requires python-config, or "
+                  "set it with PYTHON_CONFIG variable.")
         raise SCons.Errors.StopError(msg)
+    env.LogDebug("using python config: %s" % (pconfig))
 
     env.MergeFlags('!%s --includes' % (pconfig))
 
     plibs = RunConfig(env, pconfig + ' --libs')
     rx = re.search(r"-l(python\S*)", plibs)
-    if not rx:
+    plibname = None
+    if rx:
+        plibname = rx.group(1)
+    else:
         msg = ("boost_python: could not parse python library name from "
                "%s output: %s" % (pconfig + ' --libs', plibs))
-        raise SCons.Errors.StopError(msg)
-    plibname = rx.group(1)
-    env.LogDebug("found python shared library name: %s" % (plibname))
-    env.LogDebug("looking for a boost library which depends on %s" % (plibname))
-    blib = _find_boost(env, plibname)
-    if not blib:
-        msg = ("boost_python: could not find boost_python library which "
-               "depends on: %s" % (plibname))
-        raise SCons.Errors.StopError(msg)
+        env.LogDebug(msg)
+        if require_plib:
+            raise SCons.Errors.StopError(msg)
+    # Get flags to find the python library, especially LIBPATH so
+    # _find_boost can look in the right lib directories.
+    ldflags = RunConfig(env, pconfig + ' --ldflags')
+    env.LogDebug("using ldflags: %s" % (ldflags))
+    # If there is no python lib in the config output, then assume the right
+    # libboost_python will be found on the LIBPATH set by python-config.
+    # This accommodates conda environments where python3-config does not
+    # list a python library name, but the libboost_python libraries have
+    # the python version as a suffix even though without naming the shared
+    # python library as a dynamic dependency.
+    blib = 'boost_python'
+    if plibname:
+        env.LogDebug("python library name extracted from python config: %s" %
+                     (plibname))
+    else:
+        plibname = "python"
+        env.LogDebug("using default python library name: %s" % (plibname))
 
-    env.LogDebug("found boost library: %s" % (blib))
+    env.LogDebug("looking for a boost library which depends on %s" %
+                 (plibname))
+    clone = env.Clone()
+    clone.MergeFlags(ldflags)
+    blib = _find_boost(clone, plibname)
+    if not blib:
+        blib = "boost_python"
+        print("boost_python: could not find specific boost_python library "
+              "to match python library %s, using default: %s" %
+              (plibname, blib))
+    else:
+        print("using boost_python library: %s" % (blib))
+
+    # Add the python library dependencies after the boost_python library
+    # which depends on them.
     env.Append(LIBS=[blib])
-    env.MergeFlags('!%s --ldflags' % (pconfig))
+    env.MergeFlags(ldflags)
 
 
 def exists(env):
@@ -80,4 +151,3 @@ if __name__ == "__main__":
     eol_scons.debug.SetDebug(True)
     env = Environment(tools=['default'])
     generate(env)
-
