78d20c9b9322f15bcd458e9f158adc00a742d8e1
mike-dixon <dixon@ucar.edu>
2021-12-04 21:25:15 +0000

    eol_scons/tools/lrose.py - removing netcdf_c++4 and hdf5_cpp as library dependencies for lrose

--- eol_scons/tools/lrose.py
+++ eol_scons/tools/lrose.py
@@ -30,7 +30,7 @@ lroseLibs = ['dsdata', 'radar', 'Fmq', 'Spdb', 'Mdv', 'titan',
              'dsserver', 'Radx', 'Ncxx', 'rapformats',
              'euclid', 'rapmath', 'physics',
              'didss', 'toolsa', 'dataport', 'tdrp',
-             'netcdf_c++4', 'netcdf', 'hdf5', 'hdf5_cpp']
+             'netcdf', 'hdf5']
 
 def _calculate_settings(env, settings):
     # Look for LROSE under $LROSE_INSTALL_DIR, /usr/local/lrose,
