846c4515d2cef2d8cd4f8a564a1f2e5046767773
Chris Webster <cjw@ucar.edu>
2022-02-01 09:39:27 -0700

    Remove /opt/X11 includes and libs
    This seems to longer be necessary for MacOS, and actually the programs keep quitting with an X error.  This modifcation works for both MacPorts and Homebrew.

--- eol_scons/tools/openmotif.py
+++ eol_scons/tools/openmotif.py
@@ -4,9 +4,6 @@ import os, os.path
 
 def generate(env):
   env.Append(LIBS=['Xm','Xt','X11'])
-  if env['PLATFORM'] == 'darwin':
-    env.Append(CPPPATH=['/opt/X11/include'])
-    env.Append(LIBPATH=['/opt/X11/lib'])
 
 def exists(env):
     return True
