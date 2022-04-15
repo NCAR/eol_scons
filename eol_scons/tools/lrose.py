d35b331f98edfbce8b7dc008c9c4ddb81e4a374f
Chris Burghart <burghart@ucar.edu>
2022-04-15 21:56:27 +0000

    Add LROSE dependency on libpthread
    I believe that LROSE's libtoolsa is the source of this dependency...

--- eol_scons/tools/lrose.py
+++ eol_scons/tools/lrose.py
@@ -30,7 +30,7 @@ lroseLibs = ['dsdata', 'radar', 'Fmq', 'Spdb', 'Mdv', 'titan',
              'dsserver', 'Radx', 'Ncxx', 'rapformats',
              'euclid', 'rapmath', 'physics',
              'didss', 'toolsa', 'dataport', 'tdrp',
-             'netcdf', 'hdf5']
+             'netcdf', 'hdf5', 'pthread']
 
 def _calculate_settings(env, settings):
     # Look for LROSE under $LROSE_INSTALL_DIR, /usr/local/lrose,
