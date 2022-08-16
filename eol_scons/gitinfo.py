eca368d701cd7780b7758ed4306d8d8772f71649
Gary Granger <granger@ucar.edu>
2022-08-16 16:48:01 -0600

    refactor gitinfo
    remove REPO_EXTERNALS since it is not being used.
    remove REPO_WORKDIR from generated headers, since it seems safer
    to avoid leaking that info by default into a packaged header file.
    rather than two dictionaries, variable_map and GitInfo.values, use one.
    keep unset values as None, only convert to 'unknown' for output.
    add loadFromHeader() to read repo metadata from a generated header,
    for use in builds from extracted source archives, like for RPMs.
    git tools can be disabled with gitinfo=off, in which case repo info will
    be loaded from headers if they exist.
    if gitdump is named as a command-line target, then the git info for that
    directory will be printed by scons, as a way for scripts to access the
    version info which scons extracts.

--- eol_scons/gitinfo.py
+++ eol_scons/gitinfo.py
@@ -1,7 +1,6 @@
 # -*- python -*-
 """
-GitInfo generates versioning and other metadata based on a git working
-branch and its source repository.
+GitInfo collects versioning and other metadata from a git repository.
 
 The git information is based on the most recent annotated git tag 
 that can be reached through 'git describe'. This is combined
@@ -22,7 +21,6 @@ env['REPO_DATE']      Wed Dec 3 13:45:13 2014 -0700
 env['REPO_BRANCH']    develop
 env['REPO_WORKDIR']   /Users/martinc/git/aspen
 env['REPO_URL']       https://github.com/ncar/aspen.git
-env['REPO_EXTERNALS'] unknown
 env['REPO_HASH']      6badaf8a78527b248adcda4a88c0579dcb90198a
 
 Example generated header file text:
@@ -31,7 +29,6 @@ Example generated header file text:
 #define GITINFOINC
 
 #define REPO_REVISION  "V3.2-26"
-#define REPO_EXTERNALS "unknown"
 #define REPO_DATE      "Wed Dec 3 13:45:13 2014 -0700"
 #define REPO_WORKDIR   "/Users/martinc/git/aspen"
 #define REPO_URL       "https://github.com/ncar/aspen.git"
@@ -46,7 +43,9 @@ Notes:
 Git commands are used to extract the information.
 """
 
+import re
 import subprocess as sp
+from pathlib import Path
 
 # Set to 1 to enable debugging output
 _debug = 0
@@ -56,7 +55,7 @@ def pdebug(msg):
     if _debug:
         print(msg)
 
-#####################################################################
+
 class GitInfo:
     """
     Encapsulate the repository characteristics, making them available via a
@@ -66,12 +65,12 @@ class GitInfo:
 
     git describe --match [vV][0-9]*:
 
-      Looks for a tag starting with 'v' or 'V' followed by a number
-      followed by anything.  Returns string with up to three tokens:
-      V3.2-4-g3189d8e.  First token is the last matching tag on the branch.
-      Second token is the number of commits since the tag.  Third is the
-      abbreviate object name of the last commit.  If the tag points to the
-      most recent commit, then commit is '0'.
+      Looks for a tag starting with 'v' or 'V' followed by a number followed
+      by anything.  Returns string with up to three tokens: V3.2-4-g3189d8e.
+      First token is the last matching tag on the branch.  Second token is the
+      number of commits since the tag.  Third is the abbreviate object name of
+      the last commit.  If the tag points to the most recent commit, then
+      commit is '0'.
                   
     git config --get remote.origin.url:
 
@@ -89,34 +88,70 @@ class GitInfo:
 
     git status --porcelain:
 
-      Get a list of any modified or unknown files in this checkout.  These
-      are stored in the REPO_DIRTY key.  If this is not empty, then the git
+      Get a list of any modified or unknown files in this checkout.  These are
+      stored in the REPO_DIRTY key.  If this is not empty, then the git
       revision will have the suffix 'M'.
+
+    The following keys are available. If the details are not avaiable, the
+    value will be None.  Values of None are translated as 'unknown' in
+    generated header files.
+
+    REPO_TAG -
+        The most recent tag available on the branch. In keeping with git
+        tagging conventions, it is recomended that the tags on the master
+        branch follow a dot release format, such as V3.2 or V10.2.a, etc.
+
+    REPO_COMMITS -
+        Number of commits on this branch since the last tag.
+
+    REPO_REVISION -
+        REPO_TAG-REPO_COMMITS. Intended to be a friendly, incrementing
+        revision number, useful for identifying releases.
+
+    REPO_BRANCH -
+        The branch currently checked out.
+
+    REPO_EXTERNALS -
+        Unused.  Future additions may include a list of externals, or perhaps
+        the full GitInfo for all submodules.
+
+    REPO_DATE -
+        Date of the last commit on this branch.
+
+    REPO_URL -
+        URL of the origin.
+
+    REPO_WORKDIR -
+        Absolute path to the top directory of the repository.  This is not
+        written to generated files by default.
+
+    REPO_HASH -
+        Hash identifier of the last commit on this branch. The unequivocal way
+        to identify the exact source revision.
+
+    REPO_DIRTY -
+        List of modified or unknown files in this repo.
+
+    REPO_ERROR -
+        Error output from any git command that fails.  Empty string if there
+        are no errors, None if never set (ie, unknown).
     """
 
-    # The repository info keys.
-    _variable_map = {
-        'REPO_REVISION'        : None,
-        'REPO_EXTERNALS'       : None,
-        'REPO_DATE'            : None,
-        'REPO_URL'             : None,
-        'REPO_WORKDIR'         : None,
-        'REPO_ERROR'           : None,
-        'REPO_TAG'             : None,
-        'REPO_COMMITS'         : None,
-        'REPO_HASH'            : None,
-        'REPO_BRANCH'          : None,
-        'REPO_DIRTY'           : None
-        }
+    gitcmd: str
+    match: str
+    repopath: Path or None
+    header: Path or None
 
     def __init__(self, env=None, repopath=None):
         """
         Initialize a GitInfo object.  env is a dictionary which is checked for
         default values for the git path (GIT) and the regular expression to
-        match against annotated tag versions (GIT_DESCRIBE_MATCH).
-        repopath is the working directory where the git commands will be
-        run, so it effectively is the path to git repo to be queried.  The
-        default is the repo at the current working directory.
+        match against annotated tag versions (GIT_DESCRIBE_MATCH).  repopath
+        is None until repo info is loaded from a working directory, with
+        either getRepoInfo() or loadFromHeader().  Basically repopath is where
+        the git commands are run or the header exists, so it effectively is
+        the path to the git repo to be queried.  The default is the repo at
+        the current working directory.
         """
         # Specify the git command
         if env is None:
@@ -124,12 +159,22 @@ class GitInfo:
         self.gitcmd = env.get('GIT', 'git')
         self.match = env.get('GIT_DESCRIBE_MATCH', '[vV][0-9]*')
         self.repopath = repopath
+        # If loaded from a header file, this is the path to that file.
+        self.header = None
 
         # Initialize the repo values.
-        self.values = {}
-        for k in self._variable_map.keys():
-            self.values[k] = "unknown"
-        self.values['REPO_ERROR'] = ""
+        self.values = {
+            'REPO_REVISION': None,
+            'REPO_DATE': None,
+            'REPO_URL': None,
+            'REPO_WORKDIR': None,
+            'REPO_TAG': None,
+            'REPO_COMMITS': None,
+            'REPO_HASH': None,
+            'REPO_BRANCH': None,
+            'REPO_DIRTY': None,
+            'REPO_ERROR': None
+        }
 
     def _get_output(self, cmd):
         "Get command output or stderr if it fails"
@@ -157,48 +202,24 @@ class GitInfo:
         See if the commad output contains an error string.
         If no, return true
         If yes, append the string to the error message, and return false.
-        
+
         error may be modified by this function
         """
-        
         pdebug('cmd_out: ' + cmd_out)
         if 'Git error:' in cmd_out:
             # append the cmd error message
             error.append(cmd_out)
             return False
-        
         return True
-    
+
     def _git_info(self):
         """
-        Return a dictionary with entries keyed to git revision and repository information.
-        
-        The following keys will be populated. If the details are not avaiable,
-        the value will be "unknown" (except for giterror)
-        
-        REPO_TAG       = The most recent tag available on the branch. In keeping with 
-                         git tagging conventions, it is recomended that the tags on the master branch 
-                         follow a dot release format, such as V3.2 or V10.2.a, etc.
-        REPO_COMMITS   = Number of commits on this branch since the last tag.
-        REPO_REVISION  = REPO_TAG-REPO_COMMITS. Intended to be a friendly, incrementing revision number, 
-                         useful for identifying releases.
-        REPO_BRANCH    = The branch we are using.
-        REPO_EXTERNALS = 'unknown' (future additions may include a list of externals, or perhaps
-                         the full GitInfo for all submodules.
-        REPO_DATE      = Date of the last commit on this branch.
-        REPO_URL       = URL of the origin.
-        REPO_WORKDIR   = Working directory of your checked out branch.
-        REPO_HASH      = Hash identifier of the last commit on this branch. The unequivocal way to 
-                         identify the exact source revision.
-        REPO_ERROR     = Error messages encountered while building GitInfo.
-        REPO_DIRTY     = List of modified or unknown files in this repo.
+        Return a dictionary of repository information.
         """
-        
-        error        = []
+        error = []
 
         # Use the collected git details to populate the dicitionary items
         gitrevision     = None
-        gitexternalrevs = None
         gitdate         = None
         giturl          = None
         gitworkdir      = None
@@ -207,7 +228,7 @@ class GitInfo:
         githash         = None
         gitbranch       = None
         gitdirty        = None
-        
+
         # Run git describe, and extract the tag, number of commits, and
         # object name.  Extract the hyphenated fields from right to left,
         # in case the tag portion itself contains hyphens. --long ensures
@@ -219,7 +240,7 @@ class GitInfo:
             describe = cmd_out.split('-')
             gittag = '-'.join(describe[0:-2])
             gitcommits = describe[-2]
-            objname = describe[-1]
+            # objname = describe[-1]
 
         # Run git config to fetch the URL
         cmd_out = self._get_output([self.gitcmd, 'config', '--get',
@@ -251,83 +272,101 @@ class GitInfo:
         if self._cmd_out_ok(cmd_out, error):
             gitdirty = ",".join(cmd_out.splitlines())
 
-        # Derive the revision string which describes the state of the
-        # current checkout.  If the current commit exactly matches a tag,
-        # then leave off the -0.  If the checkout is not clean, add the
-        # 'M' modifier.
-        if gittag and gitcommits == '0':
-            gitrevision = gittag
-        elif gittag and gitcommits:
+        # Derive the revision string which describes the state of the current
+        # checkout.  If the current commit exactly matches a tag, then leave
+        # off the -0.  If the checkout is not clean, add the 'M' modifier.
+        gitrevision = gittag
+        if gittag and gitcommits != '0':
             gitrevision = gittag + "-" + gitcommits
         if gitrevision and gitdirty:
             gitrevision += "M"
 
-        # populate the dictionary
-        git_dict = {}
-        git_dict['REPO_REVISION']     = gitrevision or 'unknown'
-        git_dict['REPO_EXTERNALS']    = gitexternalrevs or 'unknown'
-        git_dict['REPO_DATE']         = gitdate or 'unknown'
-        git_dict['REPO_URL']          = giturl or 'unknown'
-        git_dict['REPO_WORKDIR']      = gitworkdir or 'unknown'
-        git_dict['REPO_ERROR']        = (error and str(error)) or ''
-        git_dict['REPO_TAG']          = gittag or 'unknown'
-        git_dict['REPO_COMMITS']      = gitcommits or 'unknown'
-        git_dict['REPO_HASH']         = githash or 'unknown'
-        git_dict['REPO_BRANCH']       = gitbranch or 'unknown'
-        git_dict['REPO_DIRTY'] = [gitdirty, 'unknown'][bool(gitdirty is None)]
-        
-        pdebug('GitInfo._git_info git_dict:')
-        for k, v in git_dict.items():
-            pdebug('   k,v:' + ' ' + k + ' ' + v)
-       
-        # return the dictionary
+        git_dict = {
+            'REPO_REVISION': gitrevision,
+            'REPO_DATE': gitdate,
+            'REPO_URL': giturl,
+            'REPO_WORKDIR': gitworkdir,
+            'REPO_ERROR': str(error) if error else '',
+            'REPO_TAG': gittag,
+            'REPO_COMMITS': gitcommits,
+            'REPO_HASH': githash,
+            'REPO_BRANCH': gitbranch,
+            'REPO_DIRTY': gitdirty
+        }
         return git_dict
 
-    def getRepoInfo(self):
-        """
-        Determine the various revision attributes, and assign them to 
-        self.values. 
-        
-        Return self
-        """
-        
-        # Fill in the key variables from the git info dictionary, but only
-        # if there is a value there that is not None.
-        gitdict = self._git_info()
-        
-        pdebug('GitInfo.getRepoInfo self.values:')
-        for k in self._variable_map.keys():
-            if gitdict[k] is not None:
+    def _set_values(self, gitdict):
+        pdebug('GitInfo._set_values:')
+        for k in self.values.keys():
+            if gitdict.get(k) is not None:
                 self.values[k] = gitdict[k]
                 pdebug('   k,v: ' + k + ' ' + self.values[k])
 
+    def getRepoInfo(self, repopath=None):
+        """
+        Determine the various revision attributes for the directory at
+        repopath, and assign them to self.values.  If repopath is None, use
+        the repopath set when this GitInfo was initialized.
+
+        Return self
+        """
+        if repopath is not None:
+            self.repopath = repopath
+        # Fill in the key variables from the git info dictionary, but only
+        # if there is a value there that is not None.
+        gitdict = self._git_info()
+        self._set_values(gitdict)
         return self
 
     def applyToEnv(self, env):
         "Apply the info values to the environment."
-
         for k,v in self.values.items():
             env[k] = v
+
+    def loadFromHeader(self, path):
+        """
+        Given the path to a generated header file, parse the repo info from
+        the file instead of calling git tools.
+        """
+        self.header = Path(path)
+        self.repopath = Path(path).parent
+        lines = []
+        gitdict = {}
+        msg = ''
+        try:
+            with open(path) as fin:
+                lines = fin.readlines()
+        except Exception as ex:
+            msg = ("Could not load repo info from header %s: %s" %
+                   (path, str(ex)))
+        gitdict['REPO_ERROR'] = msg
+        rxp = re.compile(r"^#define\s+(?P<key>REPO_[a-zA-Z_0-9]+)"
+                         r"\s+\"(?P<value>.+)\"$")
+        for line in lines:
+            rx = rxp.match(line)
+            if not rx:
+                continue
+            value = rx.group('value')
+            value = None if value in ['unknown', 'None'] else value
+            gitdict[rx.group('key')] = value
+        self._set_values(gitdict)
 
     def generateHeader(self):
         """
         Create C text suitable for a header file, containing the revision attributes
         included as defines.
-        
+
         The self.values dictionary provide the attributes.
-        
+
         Return the text.
         """
-        
         githeader = """
 /* %(REPO_ERROR)s */
 #ifndef GITINFOINC
 #define GITINFOINC
 
 #define REPO_REVISION  \"%(REPO_REVISION)s\"
-#define REPO_EXTERNALS \"%(REPO_EXTERNALS)s\"
 #define REPO_DATE      \"%(REPO_DATE)s\"
-#define REPO_WORKDIR   \"%(REPO_WORKDIR)s\"
 #define REPO_URL       \"%(REPO_URL)s\"
 #define REPO_HASH      \"%(REPO_HASH)s\"
 #define REPO_COMMITS   \"%(REPO_COMMITS)s\"
@@ -337,25 +376,31 @@ class GitInfo:
 
 #endif
 """
-        githeader = githeader % self.values
+        repl = {k:("unknown" if v is None else v)
+                for k, v in self.values.items()}
+        githeader = githeader % repl
         pdebug(githeader)
         return githeader
+
+    def dump(self):
+        for k, v in self.values.items():
+            v = v if v is not None else 'unknown'
+            print('%s="%s"' % (k, v))
 
 
 def main():
     import sys
     repopath = None
     if len(sys.argv) > 1:
-        repopath = sys.argv[1]
-    gitinfo = GitInfo(repopath=repopath)
-    gitinfo.getRepoInfo()
-    env = {}
-    gitinfo.applyToEnv(env)
-    for k, v in env.items():
-        print("%s=%s" % (k, v))
-    #headertxt = gitinfo.generateHeader()
-    #print("\n%s" % (headertxt))
-    
+        repopath = Path(sys.argv[1])
+    gitinfo = GitInfo()
+    if repopath and repopath.is_file():
+        gitinfo.loadFromHeader(repopath)
+    else:
+        gitinfo.getRepoInfo(repopath)
+    gitinfo.dump()
+    headertxt = gitinfo.generateHeader()
+    print("\n%s" % (headertxt))
 
 
 #####################################################################
--- eol_scons/tools/gitinfo.py
+++ eol_scons/tools/gitinfo.py
@@ -45,6 +45,8 @@ has been retained in gitinfo, as there may be a need for this later.
 import os
 
 from SCons.Builder import Builder
+from SCons.Script import BoolVariable
+from SCons.Script import COMMAND_LINE_TARGETS
 from SCons.Action import Action
 from SCons.Node import FS
 from SCons.Node.Python import Value
@@ -60,9 +62,6 @@ def pdebug(msg):
     if _debug:
         print(msg)
 
-
-#####################################################################
-# Integrate GitInfo with SCons
 
 def _get_workdir(env, source):
     """
@@ -92,7 +91,34 @@ def _get_workdir(env, source):
 _gitinfomap = {}
 
 
-def _load_gitinfo(env, workdir):
+def _create_gitinfo(env, workdir) -> GitInfo:
+    "Create a new GitInfo instance, for this workdir."
+    global _gitinfomap
+    pdebug("_create_gitinfo(%s): creating gitinfo" % (workdir))
+    ginfo = GitInfo(env)
+    _gitinfomap[workdir] = ginfo
+
+    # Wherever gitinfo is loaded, create an alias to dump it.  This is could
+    # cause surprising behavior for projects without any Default() targets,
+    # since they will automatically build this target and print the repo info,
+    # so only do this if a gitdump target appears explicitly in the
+    # command-line targets.  The intention is to make it easy for other
+    # scripts, especially packaging scripts, to query for the same version
+    # information as the build is using.  However, that same information can
+    # also be parsed from generated header files, and the output in the header
+    # files could even be extended to make it easier to parse.  So it's not
+    # clear yet if this will be useful.
+    if any(['gitdump' in target for target in COMMAND_LINE_TARGETS]):
+        def gitdump(target, source, env):
+            ginfo.dump()
+        gitdump = env.Command('%s/gitdump' % (workdir), None,
+                              Action(gitdump, cmdstr=None))
+        env.Alias('gitdump', gitdump)
+        pdebug('gitinfo: created target %s' % (str(gitdump[0])))
+    return ginfo
+
+
+def _load_gitinfo(env, workdir, target=None):
     """
     Return an instance of GitInfo for the specified directory.
 
@@ -101,22 +127,38 @@ def _load_gitinfo(env, workdir):
     global _gitinfomap
     if workdir in _gitinfomap:
         # Result already cached for this workdir
-        pdebug("_load_gitinfo: returning cached gitinfo for %s" % (workdir))
-        return _gitinfomap[workdir]
+        pdebug("_load_gitinfo: using cached gitinfo for %s" % (workdir))
+        ginfo = _gitinfomap[workdir]
+    else:
+        ginfo = _create_gitinfo(env, workdir)
 
-    # Create a new GitInfo instance, for this workdir
-    pdebug("_load_gitinfo(%s): creating gitinfo" % (workdir))
-    ginfo = GitInfo(env, workdir)
-    _gitinfomap[workdir] = ginfo.getRepoInfo()
+    # if gitinfo is enabled, load the repo info from git,
+    # otherwise load it from target if available.
+    if ginfo.repopath:
+        pdebug("...repo info already loaded...")
+    elif env.get("gitinfo", True):
+        pdebug('...loading repo info with git...')
+        ginfo.getRepoInfo(workdir)
+    elif target:
+        path = str(target[0])
+        print("Loading repo info from header: %s" % (path))
+        ginfo.loadFromHeader(path)
 
+    # always make sure any updates are applied to the environment.
+    pdebug("gitinfo: applying gitinfo to env")
+    ginfo.applyToEnv(env)
     return ginfo
 
 
 def gitinfo_emitter_value(target, source, env):
-    """Given an argument for git info in the first source, replace that
-    source with a Value() node with the git info contents."""
+    """
+    Given an argument for git info in the first source, replace that source
+    with a Value() node with the git info contents.
+    """
+    pdebug("gitinfo_emitter_value(%s,%s,%s)..." %
+           (str(target[0]), str(source[0]), str(env.Dir('.'))))
     workdir = _get_workdir(env, source)
-    gitinfo = _load_gitinfo(env, workdir)
+    gitinfo = _load_gitinfo(env, workdir, target)
     # If the git info command fails with an error, we don't
     # update the target it if exists.  Have to mark the
     # target as Precious so that scons doesn't delete it
@@ -135,15 +177,21 @@ def gitinfo_do_update_target(target, source):
 
 
 def gitinfo_action_print(target, source, env):
+    if not env.get("gitinfo", True):
+        return "gitinfo disabled, not generating %s" % (target[0])
     if gitinfo_do_update_target(target, source):
         return "Generating %s" % target[0]
-    else:
-
-        return "Not updating %s" % target[0] + " due to git error"
+    return "Not updating %s" % target[0] + " due to git error"
 
 
 def gitinfo_build_value(env, target, source):
-    "Build header based on contents in the source."
+    """
+    Build header based on contents in the source.  If gitinfo has been
+    disabled, then this does nothing.
+    """
+    pdebug("gitinfo_build_value()...")
+    if not env.get("gitinfo", True):
+        return
     if gitinfo_do_update_target(target, source):
         out = open(target[0].path, "w")
         text = source[0].get_text_contents()
@@ -163,34 +211,46 @@ def generate(env):
     """
     # We need the most current git info at this point to add it to the
     # environment.  We cannot rely on a regular builder to generate the
-    # information by running gitversion later, because by then it is too
-    # late.  To cache the version info between runs and between
-    # applications of the gitinfo tool, we use a single implicit builder
-    # for a top-level header file.
+    # information by running git later, because by then it is too late.  To
+    # cache the version info between runs and between applications of the
+    # gitinfo tool, we use a single implicit builder for a top-level header
+    # file.
 
     pdebug("gitinfo: generate()")
+    variables = env.GlobalVariables()
+    if 'gitinfo' not in variables.keys():
+        variables.Add(BoolVariable("gitinfo", help="When off, "
+                  "do not run any git tools or update any version "
+                  "headers.  "
+                  "This is useful for building from source distributions "
+                  "which already contain generated version files.",
+                  default=True))
+    variables.Update(env)
+
     gitinfobuilder = Builder(
         action=Action(gitinfo_build_value, gitinfo_action_print),
         source_factory=FS.default_fs.Entry,
         emitter=gitinfo_emitter_value)
     env['BUILDERS']['GitInfo'] = gitinfobuilder
     env['GIT'] = "git"
-    env['GITVERSION'] = "gitversion"
-    # Use the default location for the subversion Windows installer.
     if env['PLATFORM'] == 'win32':
         gitbin = r'c:\Tools\git\bin'
         env.PrependENVPath('PATH', gitbin)
-        # env['GIT'] = os.path.join(gitbin, "git")
-        # env['GITVERSION'] = os.path.join(gitbin, "gitversion")
 
     env.AddMethod(LoadGitInfo, "LoadGitInfo")
     env.LoadGitInfo('#')
 
 
-def LoadGitInfo(env, source):
+def LoadGitInfo(env, source, target=None):
+    """
+    Get the git info for the source directory, parsing the output from git
+    tools if the info has not already been cached, and set the repo info
+    variables in the environment.  If gitinfo is disabled and a header file
+    target has been given, then try to parse the git info from the target
+    instead.
+    """
     workdir = _get_workdir(env, source)
-    gitinfo = _load_gitinfo(env, workdir)
-    gitinfo.applyToEnv(env)
+    gitinfo = _load_gitinfo(env, workdir, target)
 
 
 def exists(env):
