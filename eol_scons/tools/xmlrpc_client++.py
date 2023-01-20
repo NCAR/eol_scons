7bbdfb364bcd6bedb01b51749d9050a273c7b2f6
Chris Burghart <burghart@ucar.edu>
2023-01-20 10:52:02 -0700

    Add Ubuntu customizations to xmlrpc++ tools
    The Ubuntu libxmlrpc-c++8-dev package does not include associated
    pkg-config .pc files, so I baked the names of the required libraries
    directly into these SCons tool files.

--- eol_scons/tools/xmlrpc_client++.py
+++ eol_scons/tools/xmlrpc_client++.py
@@ -2,20 +2,30 @@
 Tool for xmlrpc-c client with C++ interface.  Docs for the API are
 available at http://xmlrpc-c.sourceforge.net/.
 """
+
+import subprocess
 import sys
 import SCons.Errors
 import eol_scons.parseconfig as pc
 
+
 def generate(env):
-    # Use pkg-config to get C flags and libraries
-    #
-    # At least for CentOS 7, we need the --static flag on pkg-config to get
-    # the complete list of needed libraries for the xmlrpc-c packages
-    cmd = 'pkg-config --static --libs --cflags xmlrpc_client++'
-    if not pc.ParseConfig(env, cmd):
-        print("Error loading tool xmlrpc_client++:", sys.exc_info()[0])
-        print("Have you installed package 'xmlrpc-c-devel' (or similar)?")
-        raise SCons.Errors.StopError
+    os_is_ubuntu = subprocess.run(["grep", "-q", "Ubuntu", "/etc/os-release"]).returncode == 0
+    if (os_is_ubuntu):
+        # For Ubuntu (at least as of 22.04), there is no pkg-config file for
+        # xmlrpc_client++, so we just append the two required libraries to the
+        # environment. They should be in the default library path.
+        env.Append(LIBS = "-lxmlrpc_client++ -lxmlrpc++")
+    else:
+        # Use pkg-config to get C flags and libraries
+        #
+        # At least for CentOS 7, we need the --static flag on pkg-config to get
+        # the complete list of needed libraries for the xmlrpc-c packages
+        cmd = 'pkg-config --static --libs --cflags xmlrpc_client++'
+        if not pc.ParseConfig(env, cmd):
+            print("Error loading tool xmlrpc_client++:", sys.exc_info()[0])
+            print("Have you installed package 'xmlrpc-c-devel' (or similar)?")
+            raise SCons.Errors.StopError
 
 def exists(env):
     return pc.CheckConfig(env, 'pkg-config xmlrpc_client++')
--- eol_scons/tools/xmlrpc_server_abyss++.py
+++ eol_scons/tools/xmlrpc_server_abyss++.py
@@ -3,20 +3,28 @@ Tool for xmlrpc-c Abyss server with C++ interface.  Docs for the API
 are available at http://xmlrpc-c.sourceforge.net/.
 """
 
+import subprocess
 import sys
 import SCons.Errors
 import eol_scons.parseconfig as pc
 
 def generate(env):
-    # Use pkg-config to get C flags and libraries
-    #
-    # At least for CentOS 7, we need the --static flag on pkg-config to get
-    # the complete list of needed libraries for the xmlrpc-c packages
-    cmd = 'pkg-config --static --cflags --libs xmlrpc_server_abyss++'
-    if not pc.ParseConfig(env, cmd):
-        print("Error loading tool xmlrpc_server_abyss++:", sys.exc_info()[0])
-        print("Have you installed package 'xmlrpc-c-devel' (or similar)?")
-        raise SCons.Errors.StopError
+    os_is_ubuntu = subprocess.run(["grep", "-q", "Ubuntu", "/etc/os-release"]).returncode == 0
+    if (os_is_ubuntu):
+        # For Ubuntu (at least as of 22.04), there is no pkg-config file for
+        # xmlrpc_client++, so we just append the two required libraries to the
+        # environment. They should be in the default library path.
+        env.Append(LIBS = "-lxmlrpc_server_abyss++ -lxmlrpc_server++")
+    else:
+        # Use pkg-config to get C flags and libraries
+        #
+        # At least for CentOS 7, we need the --static flag on pkg-config to get
+        # the complete list of needed libraries for the xmlrpc-c packages
+        cmd = 'pkg-config --static --cflags --libs xmlrpc_server_abyss++'
+        if not pc.ParseConfig(env, cmd):
+            print("Error loading tool xmlrpc_server_abyss++:", sys.exc_info()[0])
+            print("Have you installed package 'xmlrpc-c-devel' (or similar)?")
+            raise SCons.Errors.StopError
 
 def exists(env):
     return pc.CheckConfig(env, 'pkg-config xmlrpc_server_abyss++')
