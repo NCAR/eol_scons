18903173d09df06141b53ef287e3d83f9e9f1010
Catherine dewerd <cdewerd@ucar.edu>
2023-01-23 10:58:37 -0700

    Remove log4cpp cruft from postgres_pq.py

--- eol_scons/tools/postgres_pq.py
+++ eol_scons/tools/postgres_pq.py
@@ -18,7 +18,3 @@ def generate(env):
 def exists(env):
     return True
 
-    if pc.CheckConfig(env, 'pkg-config --exists log4cpp'):
-        pc.ParseConfig(env, 'pkg-config --cflags --libs log4cpp')
-    else:
-        env.AppendUnique(LIBS=['log4cpp'])
