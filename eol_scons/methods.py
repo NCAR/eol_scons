0925b3a84b082fa14b4c9aa08aaf39f873211252
Gary Granger <granger@ucar.edu>
2023-11-09 16:48:53 -0700

    add ConfigureTools()
    an experiment to customize Configure context environments separately
    from construction environments, which should be used with caution
    but is preserved for now in case it can be improved.

--- eol_scons/methods.py
+++ eol_scons/methods.py
@@ -2,11 +2,15 @@
 # Copyright 2007 UCAR, NCAR, All Rights Reserved
 
 
-import os, re, glob
+import os
+import re
+import glob
 
 from SCons.Util import NodeList
 from SCons.Script import DefaultEnvironment
+from SCons.Script import Environment
 from SCons.Script import GetOption
+from SCons.Script import Export
 
 import eol_scons.debug as esd
 import eol_scons.chdir as chdir
@@ -18,9 +22,9 @@ _global_targets = {}
     These are eol_scons internal functions which should only be called as
     methods through an Environment instance.  The methods are added to the
     built-in Environment class directly, so they are available to all
-    environment instances once the eol_scons package has been imported.
-    Other methods are added to an environment instance only when a particular
-    tool has been applied; see prefixoptions.py for an example using 
+    environment instances once the eol_scons package has been imported.  Other
+    methods are added to an environment instance only when a particular tool
+    has been applied; see prefixoptions.py for an example using
     InstallLibrary() and related methods.
 """
 
@@ -56,6 +60,7 @@ def EnableInstallAlias(enabled):
 
 _print_progress = not GetOption("no_progress")
 
+
 def PrintProgress(msg):
     if _print_progress:
         print(msg)
@@ -67,6 +72,7 @@ def _PassEnv(env, regexp):
     for ek in os.environ.keys():
         if re.match(regexp, ek):
             env['ENV'][ek] = os.environ[ek]
+
 
 def _Test(self, sources, actions):
     """Create a test target and aliases for the given actions with
@@ -80,8 +86,10 @@ def _Test(self, sources, actions):
     self.Tool("testing")
     return self.DefaultTest(self.TestRun("xtest", sources, actions))
 
+
 def _ChdirActions(self, actions, cdir=None):
     return chdir.ChdirActions(self, actions, cdir)
+
 
 def _Install(env, ddir, source):
     """
@@ -99,14 +107,16 @@ def _Install(env, ddir, source):
     env.Alias('install', t)
     return t
 
+
 def _AddLibraryTarget(env, base, target):
     "Register this library target using a prefix reserved for libraries."
-    while type(base) == type(NodeList) or type(base) == type([]):
+    while type(base) is NodeList or type(base) is type([]):
         base = base[0]
     name = "lib"+str(base)
     env.AddGlobalTarget(name, target)
     return target
-    
+
+
 def _AddGlobalTarget(env, name, target):
     "Register this target under the given name."
     # Make sure we register a node and not a list, just because that has
@@ -116,14 +126,14 @@ def _AddGlobalTarget(env, name, target):
     # setting always takes precedence.
     try:
         node = target[0]
-    except(TypeError, AttributeError):
+    except (TypeError, AttributeError):
         node = target
     if name not in _global_targets:
         env.LogDebug("AddGlobalTarget: " + name + "=" + node.get_abspath())
         _global_targets[name] = node
     else:
         env.LogDebug(("%s global target already set to %s, " +
-                      "not changed to %s.") % (name, _global_targets[name], 
+                      "not changed to %s.") % (name, _global_targets[name],
                                                node.get_abspath()))
     # The "local" targets is a dictionary of target strings mapped to their
     # node.  The dictionary is assigned to a construction variable.  That
@@ -169,6 +179,7 @@ def _AppendLibrary(env, name, path=None):
         if path:
             env.Append(LIBPATH=[path])
 
+
 def _AppendSharedLibrary(env, name, path=None):
     "Add this shared library either as a local target or a link option."
     env.Append(DEPLOY_SHARED_LIBS=[name])
@@ -181,6 +192,7 @@ def _AppendSharedLibrary(env, name, path=None):
         return
     env.AppendUnique(LIBPATH=[path])
     env.AppendUnique(RPATH=[path])
+
 
 def _FindPackagePath(env, optvar, globspec, defaultpath=None):
     """Check for a package installation path matching globspec."""
@@ -235,7 +247,7 @@ def _AppendDoxref(env, ref):
     """
     # If the reference is a Doxygen target node, convert it into a
     # directory reference by stripping the html/index.html from it.
-    if type(ref) != type(""):
+    if type(ref) is not type(""):
         ref = ref.Dir('..').name
     if 'DOXREF' not in env:
         env['DOXREF'] = [ref]
@@ -249,13 +261,82 @@ def _PrintProgress(env, msg):
     PrintProgress(msg)
 
 
+# ---------------------------------------------------------------------------
+# Customizing Configure() environments separately from build environments:
+#
+# It could be useful to be able to customize the Environment used by a
+# Configure context separately from the construction Environments.  The
+# motivating example is to add -Werror to the compiler flags in the global
+# tool, to confirm the whole source tree builds without warnings.  However
+# that causes Configure checks to fail, since often they have warnings about
+# unused variables and the like.  The relax_errors tool is provided to
+# override -Werror, but there is no easy way to apply to it to all Configure()
+# contexts.  The ConfigureTools() method is an attempt to intercept calls to
+# Configure() and apply a set of tools to just the Configure context's
+# Environment.  However, that does not prove so simple, because sometimes
+# Configure() uses a copy of the Environment to run compiler checks, and
+# sometimes it doesn't.  It is difficult to make sure the Configure
+# Environment is completely sanitized of unnecessary dependencies, like in
+# LIBS, since sometimes callers want the calling Environment to be modified,
+# and sometimes they do not.  Likewise, if the Configure context's Environment
+# is not a copy, then the Configure tools can modify the construction
+# Environment.  Perhaps the use of ConfigureTools() should imply automatically
+# creating a copy of the passed environment before applying the tools.
+#
+# The current implementation here seems to work, but it seems to be fragile in
+# the face of all the different ways Configure contexts are created, so it is
+# not enabled by default.  User beware.
+#
+# The intended usage looks like the call below, added to a global tool:
+#
+#    env.ConfigureTools(['relax_errors'])
+#
+# If this someday works reliably, then it could be used to consolidate all the
+# setup that currently is done for most Configure contexts in the tools,
+# especially sanitizing the LIBS variable.
+
+def relax_errors(env):
+    "Override -Werror for this environment, if set."
+    env.Append(CCFLAGS=['-Wno-error'])
+    env.Append(CXXFLAGS=['-Wno-error'])
+
+
+# make local Configure-specific tools available
+Export(relax_errors=relax_errors)
+
+
+def _Configure(env, *args, **kw):
+    """
+    Injected Configure() method to customize environments used by configure
+    contexts.
+    """
+    print("calling custom _Configure(%s,%s)" % (args, kw))
+    conf = env._eol_scons_saved_Configure(*args, **kw)
+    for tool in env.get("CONFIGURE_TOOLS") or []:
+        print("...applying tool: %s" % (tool))
+        conf.env.Tool(tool)
+    print("_Configure done.")
+    return conf
+
+
+def _ConfigureTools(env, tools):
+    """
+    Patch in a new Configure() method which applies the given tools to the
+    Environment of the created Configure context.
+    """
+    env['CONFIGURE_TOOLS'] = tools
+    if not hasattr(env, "_eol_scons_saved_Configure"):
+        env._eol_scons_saved_Configure = env.Configure
+        env.AddMethod(_Configure, "Configure")
+
+
 def _addMethods(env):
     if hasattr(env, "_eol_scons_methods_installed"):
         env.LogDebug("environment %s already has methods added" % (env))
         return
     env._eol_scons_methods_installed = True
     env.AddMethod(_LogDebug, "LogDebug")
-    env.LogDebug("add methods to environment %s, Install=%s, new _Install=%s" % 
+    env.LogDebug("add methods to environment %s, Install=%s, new _Install=%s" %
                  (env, env.Install, _Install))
     env.AddMethod(_AddLibraryTarget, "AddLibraryTarget")
     env.AddMethod(_AddGlobalTarget, "AddGlobalTarget")
@@ -273,6 +354,8 @@ def _addMethods(env):
     env.AddMethod(_AppendDoxref, "AppendDoxref")
     env.AddMethod(_PrintProgress, "PrintProgress")
 
+    # add the method to enable and set ConfigureTools
+    env.AddMethod(_ConfigureTools, "ConfigureTools")
+
     # For backwards compatibility:
     env.AddMethod(_Create, "Create")
-
