06f56298a18fe645515893702576d4b26ed7af36
Isabel Suhr <isabels@ucar.edu>
2024-02-16 10:15:50 -0700

    First try at a netcdf-cxx4 tool that works on linux/mac/windows.

--- eol_scons/tools/netcdfcxx4.py
+++ eol_scons/tools/netcdfcxx4.py
@@ -1,9 +1,21 @@
+import eol_scons.parseconfig as pc
+import sys
 
 def generate(env):
     """
     Tool for the netcdf-cxx4 library which replaces the legacy C++ API.
     """
-    env.Append(LIBS=['netcdf_c++4'])
+    # name of library depends on which build tool was used to compile it (see
+    # github issue: https://github.com/Unidata/netcdf-cxx4/issues/113). on mac,
+    # the homebrew installation doesn't include a pkgconfig file, so stick with
+    # the library name the homebrew installation uses (until we have a reason
+    # to do otherwise). otherwise, try using pkg-config.
+    if sys.platform == 'darwin':
+        env.Append(LIBS=['netcdf-cxx4'])
+    else:
+        pc.ParseConfig(env,
+                      'pkg-config --silence-errors --cflags --libs netcdf-cxx4')
+
     env.Require('netcdf')
 
 
