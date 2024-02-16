5a3b0ceb47bd39f1f1ef069acd4b9f628d44ffd8
isabels <isabels@ucar.edu>
2024-02-16 14:31:47 -0700

    Add NETCDFCXX4DIR variable if pkg-config doesn't find it.

--- eol_scons/tools/netcdfcxx4.py
+++ eol_scons/tools/netcdfcxx4.py
@@ -1,5 +1,8 @@
 import eol_scons.parseconfig as pc
 import sys
+import os
+
+_options = None
 
 def generate(env):
     """
@@ -12,10 +15,20 @@ def generate(env):
     # to do otherwise). otherwise, try using pkg-config.
     if sys.platform == 'darwin':
         env.Append(LIBS=['netcdf-cxx4'])
+    elif pc.ParseConfig(env,
+                      'pkg-config --silence-errors --cflags --libs netcdf-cxx4'):
+        pass
     else:
-        pc.ParseConfig(env,
-                      'pkg-config --silence-errors --cflags --libs netcdf-cxx4')
-
+        # have to build manually on alma9 so it's installed in /opt/local instead
+        global _options
+        if not _options:
+            _options = env.GlobalVariables()
+            _options.Add('NETCDFCXX4DIR', 'netcdf_c++4 installation path.', '/opt/local')
+        _options.Update(env)
+        nc4dir = env['NETCDFCXX4DIR']
+        env.AppendUnique(LIBPATH=[os.path.join(nc4dir, 'lib')])
+        env.AppendUnique(CPPPATH=[os.path.join(nc4dir, 'include')])
+        env.Append(LIBS=['netcdf_c++4'])
     env.Require('netcdf')
 
 
