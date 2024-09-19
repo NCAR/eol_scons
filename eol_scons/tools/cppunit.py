9465ba6f8b4bcda7095f23120b2bd64cb39e87e7
Gary Granger <granger@ucar.edu>
2024-09-19 17:45:03 -0600

    cppunit uses pkg-config now

--- eol_scons/tools/cppunit.py
+++ eol_scons/tools/cppunit.py
@@ -5,17 +5,15 @@
 import SCons.Errors
 import eol_scons.parseconfig as pc
 
-_cmd = 'cppunit-config --cflags --libs'
+_cmd = 'pkg-config --cflags --libs cppunit'
 
 
 def generate(env):
     # Don't try here to make things unique in LIBS and CFLAGS; just do a
     # simple append
     if not pc.ParseConfig(env, _cmd, unique=False):
-        print("Unable to run cppunit-config. Cannot load tool cppunit.")
+        print("cppunit not found: %s" % (_cmd))
         raise SCons.Errors.StopError
-    # needed for FC2
-    env.AppendLibrary("dl")
 
 
 def exists(env):
