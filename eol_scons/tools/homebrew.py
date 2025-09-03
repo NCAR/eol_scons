1869acdf115ee084f5174be6e863c7989fcb9253
Chris Webster <cjw@ucar.edu>
2025-09-03 14:47:15 -0600

    Test if brew exits, if not, try port, else error out.
    Test for 'brew', else test for port command and set to default MacPorts installation.  For now I've left the env variable BREW_PREFIX so we don't need to change that everywhere...yet.

--- eol_scons/tools/homebrew.py
+++ eol_scons/tools/homebrew.py
@@ -3,22 +3,33 @@
 # This source code is licensed under the MIT license found in the LICENSE
 # file in the root directory of this source tree.
 """
-Tool to setup Homebrew for MacOS.
+Tool to determine Mac prefix.  Test for homebrew, then MacPorts.
+
 """
 
 
 import subprocess
+import sys
 
 
 def generate(env):
     if env['PLATFORM'] != 'darwin':
       return
 
-    brewPrefix = subprocess.run(['brew', '--prefix'], capture_output=True, text=True).stdout.strip()
+    try:
+      macPrefix = subprocess.run(['brew', '--prefix'], capture_output=True, text=True).stdout.strip()
+    except FileNotFoundError:
+      try:
+        macPrefix = subprocess.run(['port'], capture_output=True, text=True).stdout.strip()
+        macPrefix='/opt/local'
+      except FileNotFoundError:
+        print('macprefix: no brew or port command avaialble, unclear install prefix.')
+        sys.exit()
 
-    env['BREW_PREFIX'] = brewPrefix
-    env.PrependENVPath('PATH', brewPrefix + '/bin')
-    env.AppendUnique(FRAMEWORKPATH=[brewPrefix + '/Frameworks',])
+
+    env['BREW_PREFIX'] = macPrefix
+    env.PrependENVPath('PATH', macPrefix + '/bin')
+    env.AppendUnique(FRAMEWORKPATH=[macPrefix + '/Frameworks',])
 
 
 def exists(env):
